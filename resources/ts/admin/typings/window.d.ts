40bc72ba5ca0db6ef6bd6773ef076b51282625c2
Same Old Nick <nick@sameoldnick.com>
2023-09-07 21:37:30 -0600

    Uses typeof setTimeout for field variable

--- resources/ts/admin/typings/window.d.ts
+++ resources/ts/admin/typings/window.d.ts
@@ -3,6 +3,9 @@ declare global {
 		accessToken?: IJsonWebToken;
 		refreshToken?: IJsonWebToken;
 	}
+
+    type Timeout = ReturnType<typeof window.setTimeout> | ReturnType<typeof setTimeout>;
+    type Interval = ReturnType<typeof window.setInterval> | ReturnType<typeof setInterval>;
 }
 
 export default { };
