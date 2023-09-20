a2a8dd5b1450b843f99be0a49e7f8d316952e9c7
Same Old Nick <nick@sameoldnick.com>
2023-09-19 20:34:27 -0600

    Fixed timeout type

--- resources/ts/main/components/loader.ts
+++ resources/ts/main/components/loader.ts
@@ -2,7 +2,7 @@ import $ from "jquery";
 
 const clearLoaderCallback = () => $('.loader').removeClass('show');;
 
-let loaderTimeout: NodeJS.Timeout | undefined = setTimeout(() => {
+let loaderTimeout: ReturnType<typeof setTimeout> | undefined = setTimeout(() => {
 	clearLoaderCallback();
 	loaderTimeout = undefined;
 }, 7 * 1000);
