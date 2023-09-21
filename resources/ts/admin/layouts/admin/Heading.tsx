45db9aef273ed169598f8b4771111a4f921c9545
Same Old Nick <nick@sameoldnick.com>
2023-09-21 10:50:08 -0600

    Converted class components to functional components

--- resources/ts/admin/layouts/admin/Heading.tsx
+++ resources/ts/admin/layouts/admin/Heading.tsx
@@ -1,35 +1,23 @@
 import React from 'react';
 
-interface IProps {
+interface IProps extends React.PropsWithChildren {
     title?: string;
 }
 
-interface IState {
-}
-
-export default class Heading extends React.Component<React.PropsWithChildren<IProps>, IState> {
-    public static Title: React.FC<React.PropsWithChildren> = ({ children }) => (
+const Heading: React.FC<IProps> = ({ title, children }) => {
+    const HeadingTitle: React.FC<React.PropsWithChildren> = ({ children }) => (
         <h1 className="h3 mb-0 text-gray-800">{children}</h1>
     );
 
-    constructor(props: Readonly<IProps>) {
-        super(props);
+    return (
+        <>
+            <div className="d-sm-flex align-items-center justify-content-between mb-4">
+                {title && <HeadingTitle>{title}</HeadingTitle>}
 
-        this.state = {
-        };
-    }
-
-    public render() {
-        const { title, children } = this.props;
-
-        return (
-            <>
-                <div className="d-sm-flex align-items-center justify-content-between mb-4">
-                    {title && <Heading.Title>{title}</Heading.Title>}
-
-                    {children}
-                </div>
-            </>
-        );
-    }
+                {children}
+            </div>
+        </>
+    );
 }
+
+export default Heading;
