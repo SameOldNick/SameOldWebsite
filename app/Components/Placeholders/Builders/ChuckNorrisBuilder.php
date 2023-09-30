513dd530f78061cec83e973c678b135728889db3
Same Old Nick <nick@sameoldnick.com>
2023-09-29 21:17:35 -0600

    Added Chuck Norris placeholder

---
+++ app/Components/Placeholders/Builders/ChuckNorrisBuilder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Components\Placeholders\Builders;
+
+use Illuminate\Support\Arr;
+
+class ChuckNorrisBuilder {
+    public function __construct()
+    {
+
+    }
+
+    public function __invoke() {
+        return [
+            'chuck-norris' => function() {
+                $facts = config('chucknorris', []);
+
+                return Arr::random($facts);
+            }
+        ];
+    }
+}
