586f52469d86c2691d0f20218e4c9146471c15ce
Same Old Nick <nick@sameoldnick.com>
2023-10-01 23:18:23 -0600

    Tags can be escaped by adding extra []

--- app/Components/Placeholders/Compilers/TagCompiler.php
+++ app/Components/Placeholders/Compilers/TagCompiler.php
@@ -37,19 +37,58 @@ class TagCompiler
      */
     public function compile(string $content)
     {
-        if (! preg_match_all('/\[([^\]]+)\]/m', $content, $matches)) {
+        if (! preg_match_all('/\[{1,2}([^\]]+)\]{1,2}/m', $content, $matches)) {
             return $content;
         }
 
         $compiled = $content;
 
-        foreach ($matches[1] as $placeholder) {
-            if ($this->collection->has($placeholder)) {
-                $compiled = $this->replace($placeholder, $compiled);
+        foreach ($matches[0] as $tag) {
+            if ($this->isEscaped($tag)) {
+                $compiled = $this->escape($tag, $compiled);
+            } else {
+                $placeholder = $this->extractPlaceholder($tag);
+
+                if ($this->collection->has($placeholder)) {
+                    $compiled = $this->fill($placeholder, $compiled);
+                }
             }
         }
 
         return $compiled;
+    }
+
+    /**
+     * Checks if tag is escaped.
+     *
+     * @param string $tag
+     * @return boolean
+     */
+    protected function isEscaped(string $tag) {
+        return str_starts_with($tag, '[[') && str_ends_with($tag, ']]');
+    }
+
+    /**
+     * Escapes tag
+     *
+     * @param string $tag
+     * @param string $content
+     * @return string
+     */
+    protected function escape(string $tag, string $content) {
+        $value = substr($tag, 1, -1);
+
+        return strtr($content, [$tag => $value]);
+    }
+
+    /**
+     * Extracts placeholder from tag.
+     *
+     * @param string $tag
+     * @return string
+     */
+    protected function extractPlaceholder(string $tag) {
+        return trim($tag, '[]');
     }
 
     /**
@@ -59,7 +98,7 @@ class TagCompiler
      * @param string $content
      * @return string
      */
-    protected function replace(string $placeholder, string $content)
+    protected function fill(string $placeholder, string $content)
     {
         $tag = sprintf('[%s]', $placeholder);
 
