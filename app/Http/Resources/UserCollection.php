c7b24e535054de247bf351790da490c53bdf1484
Same Old Nick <nick@sameoldnick.com>
2023-10-14 20:08:49 -0600

    Create UserCollection.php

---
+++ app/Http/Resources/UserCollection.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class UserCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $this->collection->each->makeVisible(['created_at', 'deleted_at']);
+
+        return parent::toArray($request);
+    }
+}
