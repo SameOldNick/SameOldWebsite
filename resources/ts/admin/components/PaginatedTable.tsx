74759f1ad1f79d226eaaecc841c780a838285813
Same Old Nick <nick@sameoldnick.com>
2023-10-14 20:07:25 -0600

    Updated location of PaginateResponse

--- resources/ts/admin/components/PaginatedTable.tsx
+++ resources/ts/admin/components/PaginatedTable.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 
-import PaginateResponse from '@admin/utils/api/PaginateResponse';
+import PaginateResponse from '@admin/utils/api/models/PaginateResponse';
 
 type TChildrenCallback<TData = unknown> = (data: TData[]) => React.ReactNode;
 
