7d4afab4ee143077baa4f248ece926b18535e7e2
Same Old Nick <nick@sameoldnick.com>
2023-10-26 23:04:41 -0600

    Create SkillSeeder.php

---
+++ database/seeders/Initial/SkillSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders\Initial;
+
+use App\Models\Skill;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+
+class SkillSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $skills = [
+            [
+                'icon' => 'fas-code',
+                'skill' => 'Web Development'
+            ],
+            [
+                'icon' => 'fas-graduation-cap',
+                'skill' => 'Education'
+            ],
+            [
+                'icon' => 'fas-mobile-alt',
+                'skill' => 'Mobile Development'
+            ]
+        ];
+
+        foreach ($skills as $skill) {
+            Skill::create($skill);
+        }
+    }
+}
