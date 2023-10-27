ef1059d08a7273046234fa2330f3e5cf5a1b498a
Same Old Nick <nick@sameoldnick.com>
2023-10-26 23:37:59 -0600

    Added renderer for footer menu

---
+++ app/Components/Menus/Render/Drivers/FooterRenderer.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Components\Menus\Render\Drivers;
+
+use App\Components\Menus\Contracts\MultiLevelRenderer;
+use App\Components\Menus\Items\DropdownItem;
+use App\Components\Menus\Items\LinkItem;
+use App\Components\Menus\Items\MenuDivider;
+use App\Components\Menus\Menu;
+use Illuminate\Http\Request;
+
+class FooterRenderer implements MultiLevelRenderer
+{
+    protected $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function renderOuter(Menu $menu, string $inner)
+    {
+        return view('components.menus.footer.outer', ['attributes' => $menu->attributes(), 'inner' => $inner]);
+    }
+
+    public function renderItem(LinkItem $item, int $depth)
+    {
+        $data = ['item' => $item, 'active' => $item->getMatcher()->matches($this->request)];
+
+        return view($depth == 1 ? 'components.menus.footer.item' : 'components.menus.footer.dropdown-item', $data);
+    }
+
+    public function renderDropdown(DropdownItem $dropdown, string $inner, int $depth)
+    {
+        return view('components.menus.footer.dropdown', compact('dropdown', 'inner'));
+    }
+
+    public function renderDivider(MenuDivider $divider, int $depth)
+    {
+        return view($depth == 1 ? 'components.menus.footer.divider' : 'components.menus.footer.dropdown-divider');
+    }
+}
---
+++ resources/views/components/menus/footer/item.blade.php
@@ -0,0 +1,11 @@
+<li @class('nav-item') >
+    <a @class(['nav-link', 'active' => $active]) aria-current="page" href="{{ $item->getResolver()->resolve() }}">
+        @if ($item->hasProp('icon'))
+            <span class="me-1">
+                <i class="{{ $item->getProp('icon') }}"></i>
+            </span>
+        @endif
+
+        {{ $item->getContent() }}
+    </a>
+</li>
