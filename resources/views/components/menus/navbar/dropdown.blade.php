a10add0edf3b7a7cecaa54ea51dd6de808664581
Same Old Nick <nick@sameoldnick.com>
2023-10-26 23:36:23 -0600

    Fixed attributes for navbar menu

--- resources/views/components/menus/navbar/dropdown.blade.php
+++ resources/views/components/menus/navbar/dropdown.blade.php
@@ -1,6 +1,5 @@
-<li {{ $dropdown->attributes('li')->merge(['class' => 'nav-item dropdown']) }}>
-    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-        data-bs-toggle="dropdown" aria-expanded="false">
+<li @class('nav-item', 'dropdown')>
+    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
         {{ $dropdown->getText() }}
     </a>
     <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
--- resources/views/components/menus/navbar/item.blade.php
+++ resources/views/components/menus/navbar/item.blade.php
@@ -1,5 +1,5 @@
-<li {{ $item->attributes('li')->merge(['class' => 'nav-item']) }}>
-    <a {{ $item->attributes('a')->merge(['class' => 'nav-link' . ($active ? ' active' : '')]) }} aria-current="page" href="{{ $item->getResolver()->resolve() }}">
+<li @class('nav-item')>
+    <a @class(['nav-link', 'active' => $active]) aria-current="page" href="{{ $item->getResolver()->resolve() }}">
         @if ($item->hasProp('icon'))
             <span class="me-1">
                 <i class="{{ $item->getProp('icon') }}"></i>
