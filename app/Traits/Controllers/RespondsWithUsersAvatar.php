6a97c0f5bff965a4b6c74eeef077860eca6f9d94
Same Old Nick <nick@sameoldnick.com>
2023-10-28 12:08:25 -0600

    Responds with OAuth Provider avatar (if available)

--- app/Traits/Controllers/RespondsWithUsersAvatar.php
+++ app/Traits/Controllers/RespondsWithUsersAvatar.php
@@ -3,17 +3,44 @@
 namespace App\Traits\Controllers;
 
 use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Storage;
 
 trait RespondsWithUsersAvatar
 {
     protected function respondWithAvatar(User $user, ?int $size)
     {
-        if (! is_null($user->avatar)) {
+        if ($this->hasUploadedAvatar($user)) {
             return Storage::download($user->avatar);
+        } else if ($response = $this->respondWithOauthProviderAvatar($user, $size)) {
+            return $response;
         } else {
             return $this->respondWithDefaultAvatar($user, $size);
         }
+    }
+
+    protected function hasUploadedAvatar(User $user) {
+        return ! is_null($user->avatar);
+    }
+
+    protected function respondWithUploadedAvatar(User $user, ?int $size) {
+        return Storage::download($user->avatar);
+    }
+
+    protected function respondWithOauthProviderAvatar(User $user, ?int $size) {
+        foreach ($user->oauthProviders()->latest()->get() as $provider) {
+            if (! empty($provider->avatar_url)) {
+                $url = $provider->avatar_url;
+
+                if ($size) {
+                    $url = Str::appendQuery($url, ['s' => $size]);
+                }
+
+                return redirect()->away($url);
+            }
+        }
+
+        return null;
     }
 
     protected function respondWithDefaultAvatar(User $user, ?int $size)
