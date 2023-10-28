3f513d6dc6bbeac0623bd51581a259cbd86287a4
Same Old Nick <nick@sameoldnick.com>
2023-10-28 12:09:49 -0600

    Create pages.php

---
+++ config/pages.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'homepage' => [
+        'avatar' => [
+            /**
+             * The users ID to use for the avatar.
+             */
+            'user' => 1,
+
+            /**
+             * Extra attributes for <img> tag.
+             */
+            'attrs' => [
+                'alt' => 'Same Old Nick'
+            ]
+        ],
+    ]
+];
