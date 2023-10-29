e8e4baf37d88b2aa47ee0fcb972edba829f0d891
Same Old Nick <nick@sameoldnick.com>
2023-10-29 15:21:49 -0600

    Uses seperate menu for footer

--- app/Providers/MenuServiceProvider.php
+++ app/Providers/MenuServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Components\Menus\Facades\Menus;
+use App\Components\Menus\Items\DropdownItem;
 use App\Components\Menus\Items\LinkItem;
 use App\Components\Menus\Menu;
 use App\Models\Article;
@@ -33,10 +34,17 @@ class MenuServiceProvider extends ServiceProvider
         }
 
         Menus::create('main', function (Menu $menu) {
-            $menu
-                ->route('home', 'Home', $this->applyIcon('fa-solid fa-house'))
-                ->route('blog', 'Blog', $this->applyIcon('fa-solid fa-newspaper'))
-                ->url('/contact', 'Contact Me', $this->applyIcon('fa-solid fa-envelope'));
+            $this->createSharedMenu($menu);
+        });
+
+        Menus::create('footer', function (Menu $menu) {
+            $this->createSharedMenu($menu)
+                ->dropdown('Legal', function (DropdownItem $dropdown) {
+                    $dropdown
+                        ->icon('fa-solid fa-scale-balanced')
+                        ->route('terms-conditions', 'Terms & Conditions')
+                        ->route('privacy-policy', 'Privacy Policy');
+                });
         });
 
         Menus::create('blog.sidebar.archives', function (Menu $menu) {
@@ -51,6 +59,13 @@ class MenuServiceProvider extends ServiceProvider
                 $menu->route(['blog.archive', ['year' => $yearMonth->year, 'month' => $yearMonth->month]], $yearMonth->format('F Y'));
             }
         });
+    }
+
+    private function createSharedMenu(Menu $menu) {
+        return $menu
+            ->route('home', 'Home', $this->applyIcon('fa-solid fa-house'))
+            ->route('blog', 'Blog', $this->applyIcon('fa-solid fa-newspaper'))
+            ->url('/contact', 'Contact Me', $this->applyIcon('fa-solid fa-envelope'));
     }
 
     /**
