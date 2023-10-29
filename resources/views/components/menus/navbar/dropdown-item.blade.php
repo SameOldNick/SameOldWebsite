2b6ed15fd8c540ef10aa9d799026a1d51f684160
Same Old Nick <nick@sameoldnick.com>
2023-10-29 01:04:11 -0600

    Icons can be applied to dropdowns

--- resources/views/components/menus/navbar/dropdown-item.blade.php
+++ resources/views/components/menus/navbar/dropdown-item.blade.php
@@ -1,3 +1,11 @@
 <li>
-    <a @class(['dropdown-item', 'active' => $active]) aria-current="page" href="{{ $item->getResolver()->resolve() }}">{{ $item->getContent() }}</a>
+    <a @class(['dropdown-item', 'active' => $active]) aria-current="page" href="{{ $item->getResolver()->resolve() }}">
+        @if ($item->hasProp('icon'))
+            <span class="me-1">
+                <i class="{{ $item->getProp('icon') }}"></i>
+            </span>
+        @endif
+
+        {{ $item->getContent() }}
+    </a>
 </li>
