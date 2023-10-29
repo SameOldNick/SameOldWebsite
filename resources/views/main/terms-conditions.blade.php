42ee6bb7641d2a9aa0966b795040ea9b182d99cb
Same Old Nick <nick@sameoldnick.com>
2023-10-29 15:22:39 -0600

    Added terms and conditions page and privacy policy page

---
+++ resources/views/main/terms-conditions.blade.php
@@ -0,0 +1,47 @@
+<x-main.layout class="container my-5">
+    <x-slot:title>{{ __('Terms and Conditions') }}</x-slot:title>
+
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-8">
+            <div class="card card-container shadow-lg bg-light mt-3">
+
+                <div class="card-header card-header-banner">
+                    <h2 class="text-center">{{ __('Terms and Conditions') }}</h2>
+                </div>
+                <div class="card-body">
+                    <h3>{{ __('Acceptance of Terms') }}</h3>
+                    <p>By using this website, you agree to comply with and be bound by these terms and conditions. If you do not agree with any part of these terms, please refrain from using the website.</p>
+
+                    <h3>{{ __('Intellectual Property') }}</h3>
+                    <p>All content on this website, including text, images, and any other materials, are protected by copyright and other intellectual property rights. You may only use the content for personal, non-commercial purposes.</p>
+
+                    <h3>{{ __('User Conduct') }}</h3>
+                    <p>You agree not to engage in any activity that may disrupt the website's functionality or compromise its security. This includes not attempting to gain unauthorized access to the website.</p>
+
+                    <h3>{{ __('Privacy Policy') }}</h3>
+                    <p>Your use of this website is also governed by our Privacy Policy. Please review it to understand how we collect, use, and protect your personal information.</p>
+
+                    <h3>{{ __('User-Generated Content') }}</h3>
+                    <p>If you submit content to the website (e.g., comments or posts), you are responsible for the content you provide. You agree not to post any material that is unlawful, defamatory, or infringing on the rights of others.</p>
+
+                    <h3>{{ __('Links to Third-Party Websites') }}</h3>
+                    <p>This website may contain links to external websites. We do not endorse or have control over the content on these third-party sites and are not responsible for any damages or issues resulting from their use.</p>
+
+                    <h3>{{ __('Limitation of Liability') }}</h3>
+                    <p>We are not liable for any damages or losses arising from your use of this website. We make no warranties, express or implied, regarding the website's accuracy, reliability, or fitness for a particular purpose.</p>
+
+                    <h3>{{ __('Changes to Terms') }}</h3>
+                    <p>We may modify these terms and conditions at any time. By continuing to use the website after any changes, you agree to be bound by the revised terms.</p>
+
+                    <h3>{{ __('Termination') }}</h3>
+                    <p>We reserve the right to terminate or suspend your access to the website at our discretion, without notice, if you violate these terms and conditions.</p>
+
+                    <h3>{{ __('Governing Law') }}</h3>
+                    <p>These terms and conditions are governed by the laws of Alberta, Canada, and any disputes will be resolved in the appropriate courts of that jurisdiction.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-main.layout>
+
+
