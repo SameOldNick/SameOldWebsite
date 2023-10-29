27e4b2c9488228311027ab1b0f7a114e2d7e34aa
Same Old Nick <nick@sameoldnick.com>
2023-10-29 01:04:11 -0600

    Icons can be applied to dropdowns

--- resources/views/components/menus/navbar/dropdown.blade.php
+++ resources/views/components/menus/navbar/dropdown.blade.php
@@ -1,5 +1,11 @@
 <li @class('nav-item', 'dropdown')>
     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        @if ($dropdown->hasProp('icon'))
+            <span class="me-1">
+                <i class="{{ $dropdown->getProp('icon') }}"></i>
+            </span>
+        @endif
+
         {{ $dropdown->getText() }}
     </a>
     <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
