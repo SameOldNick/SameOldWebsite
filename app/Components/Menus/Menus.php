0a0658ae81daaaa3d4e9b375b0fc9909f60d2e09
Same Old Nick <nick@sameoldnick.com>
2023-11-27 00:11:45 -0700

    Menus can be lazily created

--- app/Components/Menus/Menus.php
+++ app/Components/Menus/Menus.php
@@ -6,11 +6,14 @@ use OutOfBoundsException;
 
 class Menus
 {
+    protected $callbacks;
+
     protected $menus;
 
     public function __construct()
     {
         $this->menus = collect();
+        $this->callbacks = [];
     }
 
     /**
@@ -20,13 +23,27 @@ class Menus
      * @param callable $callback Called with Menu instance
      * @return Menu
      */
-    public function create($name, callable $callback)
+    public function create(string $name, callable $callback)
     {
         $this->menus[$name] = new Menu();
 
         $callback($this->menus[$name]);
 
         return $this->menus[$name];
+    }
+
+    /**
+     * Lazily creates a menu.
+     *
+     * @param string $name
+     * @param callable $callback Called when menu is first accessed.
+     * @return $this
+     */
+    public function lazyCreate(string $name, callable $callback)
+    {
+        $this->callbacks[$name] = $callback;
+
+        return $this;
     }
 
     /**
@@ -38,6 +55,10 @@ class Menus
      */
     public function get($name)
     {
+        if (isset($this->callbacks[$name])) {
+            $this->create($name, $this->callbacks[$name]);
+        }
+
         if (! $this->menus->has($name)) {
             throw new OutOfBoundsException("Menu '{$name}' does not exist.");
         }
