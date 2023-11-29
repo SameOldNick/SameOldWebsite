b04af15f2565d1fb075d63c9c6461ab7de56b76b
Same Old Nick <nick@sameoldnick.com>
2023-11-28 23:25:19 -0700

    Create encryption.php

---
+++ config/encryption.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'defaults' => [
+        'signer' => 'ecdsa'
+    ],
+
+    'drivers' => [
+        'ecdsa' => [
+            /**
+             * Type of key.
+             * Possible values: 'file' or 'string'
+             */
+            'type' => env('ENCRYPTION_ECDSA_KEY_TYPE', 'file'),
+
+            /**
+             * Path to private key (if type is 'file').
+             * The file contents must be in PEM format.
+             */
+            'path' => env('ENCRYPTION_ECDSA_KEY_PATH'),
+
+            /**
+             * Private key contents (if type is 'string').
+             * The contents must be in PEM format.
+             */
+            'contents' => env('ENCRYPTION_ECDSA_KEY_CONTENTS'),
+
+
+        ]
+    ]
+];
