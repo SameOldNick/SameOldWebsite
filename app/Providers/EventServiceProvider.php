a0cf80146757d5e9cdef975bc4c3f173e8a56e91
Same Old Nick <nick@sameoldnick.com>
2023-11-29 08:10:09 -0700

    Added missing use statements

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,11 +2,14 @@
 
 namespace App\Providers;
 
-use App\Events\Comments\CommentApproved;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+
+use App\Events\Comments\CommentApproved;
+use App\Listeners\NotifyArticleAuthorCommentPosted;
+use App\Listeners\NotifyCommentRepliedTo;
 
 class EventServiceProvider extends ServiceProvider
 {
