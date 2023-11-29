9c3bcecf49fad7275eaa329a2792534792aeff72
Same Old Nick <nick@sameoldnick.com>
2023-11-28 23:19:24 -0700

    Removed settings provider

--- app/Providers/ComponentServiceProvider.php
+++ app/Providers/ComponentServiceProvider.php
@@ -9,7 +9,6 @@ use App\Components\LittleJWT\ServiceProvider as LittleJWTServiceProvider;
 use App\Components\Macros\ServiceProvider as MacrosServiceProvider;
 use App\Components\Menus\ServiceProvider as MenusServiceProvider;
 use App\Components\OAuth\ServiceProvider as OAuthServiceProvider;
-use App\Components\Settings\ServiceProvider as SettingsServiceProvider;
 use App\Components\SweetAlert\ServiceProvider as SweetAlertServiceProvider;
 use Illuminate\Support\AggregateServiceProvider;
 
@@ -26,7 +25,6 @@ class ComponentServiceProvider extends AggregateServiceProvider
         MacrosServiceProvider::class,
         CompilerServiceProvider::class,
         LittleJWTServiceProvider::class,
-        SettingsServiceProvider::class,
         OAuthServiceProvider::class,
         AnalyticsServiceProvider::class,
         EncryptionServiceProvider::class,
