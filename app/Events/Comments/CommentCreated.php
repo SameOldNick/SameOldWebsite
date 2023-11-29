8ccd3a8e9545adf5f1d7d08bb9465a1d8a0afc49
Same Old Nick <nick@sameoldnick.com>
2023-11-28 23:24:28 -0700

    Events are fired when comment is created, approved, or removed

---
+++ app/Events/Comments/CommentCreated.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Events\Comments;
+
+use App\Models\Comment;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class CommentCreated
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(
+        public readonly Comment $comment
+    )
+    {
+        //
+    }
+}
