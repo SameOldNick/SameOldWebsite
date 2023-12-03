2ebc6d5463e5f84e1b92d61e54b93a1b69f8ce39
Same Old Nick <nick@sameoldnick.com>
2023-12-02 22:55:20 -0700

    Uses TestSeeder

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,24 @@
 
 namespace Tests;
 
+use Database\Seeders\TestSeeder;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Indicates whether the default seeder should run before each test.
+     *
+     * @var bool
+     */
+    protected $seed = true;
+
+    /**
+     * The specific seeder class that should be used when refreshing the database.
+     *
+     * @var string
+     */
+    protected $seeder = TestSeeder::class;
 }
