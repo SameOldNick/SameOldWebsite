8c67579d2acf5c5bfc0f18e44fa414248b916294
Same Old Nick <nick@sameoldnick.com>
2023-12-02 21:52:58 -0700

    Uses withRevision method to generate revisions

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\Article;
 use App\Models\Post;
 use App\Models\User;
+use App\Models\Revision;
 use App\Traits\Database\Factories\CreatesPostable;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
@@ -39,6 +40,17 @@ class ArticleFactory extends Factory
     public function configure()
     {
         return $this;
+    }
+
+    /**
+     * Includes revision(s) with article.
+     *
+     * @param integer $count
+     * @return $this
+     */
+    public function withRevision(int $count = 1)
+    {
+        return $this->has(Revision::factory()->count($count), 'revisions');
     }
     }
 
