c96362b15a0472fc5511d7ad8cf5443651032fd4
Same Old Nick <nick@sameoldnick.com>
2023-12-02 22:53:39 -0700

    Create CreatesUser.php

---
+++ tests/Feature/Traits/CreatesUser.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature\Traits;
+
+use App\Models\User;
+
+trait CreatesUser
+{
+    protected $user;
+
+    public function setUpCreatesUser()
+    {
+        $this->user = User::factory()->create();
+    }
+
+
+    public function tearDownCreatesUser()
+    {
+
+    }
+}
