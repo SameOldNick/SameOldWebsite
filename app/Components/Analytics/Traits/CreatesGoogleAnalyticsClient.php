6e7074873d19414414b64fc2fb932f680ca0fbf1
Same Old Nick <nick@sameoldnick.com>
2023-12-15 23:51:00 -0700

    Added PHPDoc

--- app/Components/Analytics/Traits/CreatesGoogleAnalyticsClient.php
+++ app/Components/Analytics/Traits/CreatesGoogleAnalyticsClient.php
@@ -6,11 +6,21 @@ use Google\Analytics\Data\V1beta\Client\BetaAnalyticsDataClient;
 
 trait CreatesGoogleAnalyticsClient
 {
+    /**
+     * Gets the Google Analytics property ID
+     *
+     * @return string
+     */
     protected function getPropertyId()
     {
         return config('services.google.analytics.property_id');
     }
 
+    /**
+     * Creates Google Analytics data client.
+     *
+     * @return BetaAnalyticsDataClient
+     */
     protected function createDataClient()
     {
         return new BetaAnalyticsDataClient([
