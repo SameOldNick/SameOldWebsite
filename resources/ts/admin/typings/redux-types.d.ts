d58dcab84846bf75807c9aa9092aa65c60090536
Same Old Nick <nick@sameoldnick.com>
2023-12-16 16:55:50 -0700

    Removed LocationState

--- resources/ts/admin/typings/redux-types.d.ts
+++ resources/ts/admin/typings/redux-types.d.ts
@@ -3,5 +3,4 @@ import store from "@admin/store/index";
 declare global {
     export type RootState = ReturnType<typeof store.getState>;
     export type AppDispatch = typeof store.dispatch;
-    export type LocationState = { };
 }
