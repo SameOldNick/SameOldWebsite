ec244b4af031d946c56c42057fa9857508f7bb62
Same Old Nick <nick@sameoldnick.com>
2024-02-04 23:22:48 -0700

    MFA is required to access admin area
    All admin routes are protected by middleware

--- routes/admin.php
+++ routes/admin.php
@@ -3,8 +3,12 @@
 use App\Http\Controllers\Admin;
 use Illuminate\Support\Facades\Route;
 
-Route::namespace(Admin::class)->group(function () {
-    Route::middleware(['web', 'signed', 'can:admin'])->get('/sso/{user}', [Admin\AdminController::class, 'singleSignOn'])->name('sso');
+Route::namespace(Admin::class)->middleware(['web', 'auth.mfa', 'can:admin'])->group(function () {
+    /**
+     * This controller method is specifically requires signing because it provides the JWTs.
+     * Once the JWTs are provided, there's nothing stopping a user from using them.
+     */
+    Route::middleware(['signed'])->get('/sso/{user}', [Admin\AdminController::class, 'singleSignOn'])->name('sso');
 
     Route::get('/', [Admin\AdminController::class, 'app']);
     Route::get('/{slug}', [Admin\AdminController::class, 'app'])->where('slug', '([A-z\d\-\/_.]+)?');
