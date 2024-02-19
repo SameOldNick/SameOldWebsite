1a529983d08f651ebe1d7c61cf29ba7c843fa5eb
Same Old Nick <nick@sameoldnick.com>
2024-02-18 22:30:06 -0700

    Fixed error page layout

--- resources/views/errors/layouts/full.blade.php
+++ resources/views/errors/layouts/full.blade.php
@@ -1,23 +1,26 @@
-<x-main.layout class="container my-5">
+<x-main.layout>
     <x-slot:title>@yield('title')</x-slot:title>
 
-    <div class="row justify-content-center">
-        <div class="col-12 col-md-8">
-            <div class="card card-container shadow-lg mt-3">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-8">
+                <div class="card card-container shadow-lg mt-3">
 
-                <div class="card-header card-header-banner">
-                    <h2 class="text-center">
-                        <span>Error @yield('code') &mdash;</span>
-                        <span>@yield('title')</span>
-                    </h2>
-                </div>
-                <div class="card-body">
-                    <div class="text-center">
-                        <p>@yield('message', __('An unknown error occurred.'))</p>
+                    <div class="card-header card-header-banner">
+                        <h2 class="text-center">
+                            <span>Error @yield('code') &mdash;</span>
+                            <span>@yield('title')</span>
+                        </h2>
+                    </div>
+                    <div class="card-body">
+                        <div class="text-center">
+                            <p>@yield('message', __('An unknown error occurred.'))</p>
 
-                        @yield('content')
+                            @yield('content')
 
-                        <a href="{{ route('home') }}" class="btn btn-secondary mt-3">{{ __('Back to Home') }}</a>
+                            <a href="javascript: window.history.go(-1)" class="btn btn-secondary mt-3">{{ __('Go Back') }}</a>
+                            <a href="{{ route('home') }}" class="btn btn-secondary mt-3">{{ __('Back to Home') }}</a>
+                        </div>
                     </div>
                 </div>
             </div>
