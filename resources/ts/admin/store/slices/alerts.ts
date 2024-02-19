32005b09d7708b41ca69a7bc0e3e9fb5675cf6df
Same Old Nick <nick@sameoldnick.com>
2024-02-18 22:56:21 -0700

    merge is no longer the default export

--- resources/ts/admin/store/slices/alerts.ts
+++ resources/ts/admin/store/slices/alerts.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import merge from "ts-deepmerge";
+import { merge } from "ts-deepmerge";
 import { v4 as uuidv4 } from 'uuid';
 import { SweetAlertOptions } from "sweetalert2";
 
