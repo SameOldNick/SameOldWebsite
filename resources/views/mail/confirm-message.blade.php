686a52d56cc616c4166be22656e0f71033ec0c22
Same Old Nick <nick@sameoldnick.com>
2024-02-18 20:44:15 -0700

    Fixed up confirm message

--- resources/views/mail/confirm-message.blade.php
+++ resources/views/mail/confirm-message.blade.php
@@ -1,9 +1,12 @@
 <x-mail::message>
-You must confirm your e-mail address to send a message.
+Thank you for your message!
+To complete your contact form submission, please confirm your email address by clicking the link below:
 
 <x-mail::button :url="$url">
 Verify E-mail Address
 </x-mail::button>
+
+If you did not make a contact form submission, please disregard this email.
 
 Thanks,<br>
 {{ config('app.name') }}
