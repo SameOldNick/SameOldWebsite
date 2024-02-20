93967999d00bf775927c3b2b94c0929db2711e98
Same Old Nick <nick@sameoldnick.com>
2024-02-19 20:31:39 -0700

    Allows JWT to be included for user

---
+++ tests/Feature/Traits/InteractsWithJWT.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature\Traits;
+
+use Illuminate\Contracts\Auth\Authenticatable;
+use LittleApps\LittleJWT\JWT\JWT;
+
+trait InteractsWithJWT {
+    /**
+	 * Includes JWT in HTTP requests
+	 *
+	 * @param string|JWT $token
+	 * @return static
+	 */
+	public function withJwt($token) {
+		return $this->withHeader('Authorization', sprintf('Bearer %s', (string) $token));
+	}
+
+	/**
+     * Set the currently logged in user for the application.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
+     * @param  string|null  $driver
+     * @return $this
+     */
+	public function actingAs(Authenticatable $user, $driver = null) {
+        $accessToken = $this->app['auth']->guard('jwt')->buildJwtForUser($user);
+
+		$this->app['auth']->guard($driver)->setUser($user);
+		$this->app['auth']->shouldUse($driver);
+
+		return $this->withJwt($accessToken);
+	}
+}
