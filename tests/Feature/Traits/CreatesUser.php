a9fff5713725d0cdeabd343530fd791cf97c73d9
Same Old Nick <nick@sameoldnick.com>
2024-02-19 20:30:57 -0700

    Creates regular and admin user

--- tests/Feature/Traits/CreatesUser.php
+++ tests/Feature/Traits/CreatesUser.php
@@ -3,14 +3,36 @@
 namespace Tests\Feature\Traits;
 
 use App\Models\User;
+use App\Models\Role;
 
 trait CreatesUser
 {
+    /**
+     * A regular user.
+     *
+     * @var User
+     */
     protected $user;
 
+    /**
+     * An admin user.
+     *
+     * @var User
+     */
+    protected $admin;
+
+    /**
+     * Creates the users.
+     *
+     * @return void
+     */
     public function setUpCreatesUser()
     {
         $this->user = User::factory()->create();
+
+        $this->admin = tap(User::factory()->create(), function ($user) {
+            $user->roles()->attach(Role::firstWhere('role', 'admin'));
+        });
     }
 
     public function tearDownCreatesUser()
