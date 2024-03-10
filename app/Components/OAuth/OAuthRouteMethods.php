658760dfb72e6bf73d79bcfc8d50d64c4a1be8aa
Same Old Nick <nick@sameoldnick.com>
2024-03-09 23:33:16 -0700

    Type hints Router

--- app/Components/OAuth/OAuthRouteMethods.php
+++ app/Components/OAuth/OAuthRouteMethods.php
@@ -7,10 +7,12 @@ class OAuthRouteMethods
     public function oauth()
     {
         return function ($options = []) {
+            /** @var \Illuminate\Routing\Router $this */
             $this->group([
                 'namespace' => class_exists($this->prependGroupNamespace('Auth\OAuthController')) ? null : 'App\Http\Controllers',
                 'prefix' => 'oauth',
             ], function () use ($options) {
+                /** @var \Illuminate\Routing\Router $this */
                 $oauth = $this->container->make(OAuth::class);
 
                 foreach ($oauth->configured() as $driver) {
