483ae93609232e31255ce8950705e75afc78ddae
Same Old Nick <nick@sameoldnick.com>
2024-03-11 14:08:14 -0600

    Converted to functional component

--- resources/ts/admin/pages/protected/contact/Messages.tsx
+++ resources/ts/admin/pages/protected/contact/Messages.tsx
@@ -30,206 +30,194 @@ interface IModalProps {
     onClose: () => void;
 }
 
-interface IState {
-    showNotification?: TMessageNotification;
+const MessageRow: React.FC<IRowProps> = ({ notification, onViewClicked, onMarkReadClicked, onMarkUnreadClicked }) => {
+    const address = notification.data.addresses.replyTo.length > 0 ? notification.data.addresses.replyTo[0].address : '(unknown)';
+    const message = S(notification.data.view.text).truncate(75).s;
+
+    const isRead = React.useMemo(() => notification.read_at !== null, [notification]);
+    const styles: React.CSSProperties = !isRead ? { fontWeight: 'bold' } : {};
+
+    return (
+        <tr>
+            <td style={styles}>{notification.id}</td>
+            <td style={styles}>{address}</td>
+            <td style={styles}>{message}</td>
+            <td>
+                <Button color='primary' className='me-1' onClick={onViewClicked}>
+                    <FaExternalLinkAlt />
+                </Button>
+                {
+                    !isRead ? (
+                        <Button color='primary' title='Mark as unread' onClick={onMarkReadClicked}>
+                            <FaEnvelopeOpen />
+                        </Button>
+                    ) : (
+                        <Button color='primary' title='Mark as read' onClick={onMarkUnreadClicked}>
+                            <FaEnvelope />
+                        </Button>
+                    )
+                }
+
+            </td>
+
+        </tr>
+    );
 }
 
-export default connector(class Messages extends React.Component<TProps, IState> {
-    static Row: React.FC<IRowProps> = ({ notification, onViewClicked, onMarkReadClicked, onMarkUnreadClicked }) => {
-        const address = notification.data.addresses.replyTo.length > 0 ? notification.data.addresses.replyTo[0].address : '(unknown)';
-        const message = S(notification.data.view.text).truncate(75).s;
+const MessageModal: React.FC<IModalProps> = ({ notification, onClose }) => {
+    const toggle = () => onClose();
 
-        const isRead = React.useMemo(() => notification.read_at !== null, [notification]);
-        const styles: React.CSSProperties = !isRead ? { fontWeight: 'bold' } : {};
+    return (
+        <Modal isOpen={true} toggle={toggle} scrollable size='xl'>
+            <ModalHeader>Message</ModalHeader>
+            <ModalBody>
+                {Object.entries(notification.data.addresses).map(([label, addresses], index) => {
+                    return addresses.length > 0 && (
+                        <FormGroup key={index} tag="fieldset">
+                            <legend>{S(label).humanize().s}</legend>
 
-        return (
-            <tr>
-                <td style={styles}>{notification.id}</td>
-                <td style={styles}>{address}</td>
-                <td style={styles}>{message}</td>
-                <td>
-                    <Button color='primary' className='me-1' onClick={onViewClicked}>
-                        <FaExternalLinkAlt />
-                    </Button>
-                    {
-                        !isRead ? (
-                            <Button color='primary' title='Mark as unread' onClick={onMarkReadClicked}>
-                                <FaEnvelopeOpen />
-                            </Button>
-                        ) : (
-                            <Button color='primary' title='Mark as read' onClick={onMarkUnreadClicked}>
-                                <FaEnvelope />
-                            </Button>
-                        )
-                    }
-
-                </td>
-
-            </tr>
-        );
-    }
-
-    static Modal: React.FC<IModalProps> = ({ notification, onClose }) => {
-        const toggle = () => onClose();
-
-        return (
-            <Modal isOpen={true} toggle={toggle} scrollable size='xl'>
-                <ModalHeader>Message</ModalHeader>
-                <ModalBody>
-                    {Object.entries(notification.data.addresses).map(([label, addresses], index) => {
-                        return addresses.length > 0 && (
-                            <FormGroup key={index} tag="fieldset">
-                                <legend>{S(label).humanize().s}</legend>
-
-                                {addresses.map(({ address, name }, index) => (
-                                    <Row key={index}>
-                                        <Col xs={6}>
-                                            <FormGroup>
-                                                <Label for='subject'>E-mail:</Label>
-                                                <Input id='subject' type='text' value={address} readOnly />
-                                            </FormGroup>
-                                        </Col>
-                                        <Col xs={6}>
-                                            <FormGroup>
-                                                <Label for='subject'>Name:</Label>
-                                                <Input id='subject' type='text' value={name || ''} readOnly />
-                                            </FormGroup>
-                                        </Col>
-                                    </Row>
-                                ))}
+                            {addresses.map(({ address, name }, index) => (
+                                <Row key={index}>
+                                    <Col xs={6}>
+                                        <FormGroup>
+                                            <Label for='subject'>E-mail:</Label>
+                                            <Input id='subject' type='text' value={address} readOnly />
+                                        </FormGroup>
+                                    </Col>
+                                    <Col xs={6}>
+                                        <FormGroup>
+                                            <Label for='subject'>Name:</Label>
+                                            <Input id='subject' type='text' value={name || ''} readOnly />
+                                        </FormGroup>
+                                    </Col>
+                                </Row>
+                            ))}
 
 
-                            </FormGroup>
-                        );
-                    })}
-
-                    <Row>
-                        <FormGroup>
-                            <Label for='subject'>Subject:</Label>
-                            <Input id='subject' type='text' value={notification.data.subject} readOnly />
                         </FormGroup>
-                    </Row>
+                    );
+                })}
 
-                    <Row>
-                        <FormGroup>
-                            <Label for='textMessage'>Text Message:</Label>
-                            <Input id='textMessage' type='textarea' value={notification.data.view.text} readOnly rows={6} />
-                        </FormGroup>
-                    </Row>
+                <Row>
+                    <FormGroup>
+                        <Label for='subject'>Subject:</Label>
+                        <Input id='subject' type='text' value={notification.data.subject} readOnly />
+                    </FormGroup>
+                </Row>
 
-                    <Row>
-                        <FormGroup>
-                            <Label for='htmlMessage'>HTML Message:</Label>
-                            <Input id='htmlMessage' type='textarea' value={notification.data.view.html} readOnly rows={6} />
-                        </FormGroup>
-                    </Row>
+                <Row>
+                    <FormGroup>
+                        <Label for='textMessage'>Text Message:</Label>
+                        <Input id='textMessage' type='textarea' value={notification.data.view.text} readOnly rows={6} />
+                    </FormGroup>
+                </Row>
 
-                </ModalBody>
+                <Row>
+                    <FormGroup>
+                        <Label for='htmlMessage'>HTML Message:</Label>
+                        <Input id='htmlMessage' type='textarea' value={notification.data.view.html} readOnly rows={6} />
+                    </FormGroup>
+                </Row>
 
-                <ModalFooter>
-                    <Button color='primary' onClick={toggle}>Close</Button>
-                </ModalFooter>
-            </Modal >
-        );
+            </ModalBody>
+
+            <ModalFooter>
+                <Button color='primary' onClick={toggle}>Close</Button>
+            </ModalFooter>
+        </Modal >
+    );
+}
+
+const Messages: React.FC<TProps> = (props) => {
+    const [showNotification, setShowNotification] = React.useState<TMessageNotification | undefined>();
+
+    const fetchMessages = async () => {
+        await props.fetchMessages();
     }
 
-    constructor(props: Readonly<TProps>) {
-        super(props);
-
-        this.state = {
-        };
+    const viewNotification = (notification: TMessageNotification) => {
+        setShowNotification(notification);
     }
 
-    componentDidMount(): void {
-        this.fetchMessages();
-    }
-
-    private async fetchMessages() {
-        await this.props.fetchMessages();
-    }
-
-    private viewNotification(notification: TMessageNotification) {
-        this.setState({ showNotification: notification });
-    }
-
-    private async markUnread(notification: TMessageNotification) {
+    const markUnread = async (notification: TMessageNotification) => {
         try {
             await createAuthRequest().post<TMessageNotification>(`/user/notifications/${notification.id}/unread`, {});
 
-            await this.fetchMessages();
+            await fetchMessages();
         } catch (err) {
             console.error(err);
         }
     }
 
-    private async markRead(notification: TMessageNotification) {
+    const markRead = async (notification: TMessageNotification) => {
         try {
             await createAuthRequest().post<TMessageNotification>(`/user/notifications/${notification.id}/read`, {});
 
-            await this.fetchMessages();
+            await fetchMessages();
         } catch (err) {
             console.error(err);
         }
     }
 
-    public render() {
-        const { stored } = this.props;
-        const { showNotification } = this.state;
+    React.useEffect(() => {
+        fetchMessages();
+    }, []);
 
-        return (
-            <>
-                <Helmet>
-                    <title>Messages</title>
-                </Helmet>
+    return (
+        <>
+            <Helmet>
+                <title>Messages</title>
+            </Helmet>
 
-                <Heading title='Messages' />
+            <Heading title='Messages' />
 
-                {showNotification && <Messages.Modal notification={showNotification} onClose={() => this.setState({ showNotification: undefined })} />}
+            {showNotification && <MessageModal notification={showNotification} onClose={() => setShowNotification(undefined)} />}
 
-                <Card>
-                    <CardBody>
-                        <Row>
-                            <Col xs={12} className='d-flex justify-content-between mb-3'>
-                                <div></div>
-                                <div className="text-end">
-                                    <Form className="row row-cols-lg-auto g-3" onSubmit={() => this.fetchMessages()}>
-                                        <Col xs={12}>
-                                            <Button type='submit' color='primary'>
-                                                <FaSync /> Update
-                                            </Button>
-                                        </Col>
-                                    </Form>
+            <Card>
+                <CardBody>
+                    <Row>
+                        <Col xs={12} className='d-flex justify-content-between mb-3'>
+                            <div></div>
+                            <div className="text-end">
+                                <Form className="row row-cols-lg-auto g-3" onSubmit={() => fetchMessages()}>
+                                    <Col xs={12}>
+                                        <Button type='submit' color='primary'>
+                                            <FaSync /> Update
+                                        </Button>
+                                    </Col>
+                                </Form>
 
-                                </div>
-                            </Col>
-                            <Col xs={12}>
-                                <Table>
-                                    <thead>
-                                        <tr>
-                                            <th>ID</th>
-                                            <th>From</th>
-                                            <th>Message</th>
-                                            <th>Actions</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {stored.map((notification, index) => (
-                                            <Messages.Row
-                                                key={index}
-                                                notification={notification}
-                                                onViewClicked={() => this.viewNotification(notification)}
-                                                onMarkReadClicked={() => this.markRead(notification)}
-                                                onMarkUnreadClicked={() => this.markUnread(notification)}
-                                            />
-                                        ))}
-                                    </tbody>
-                                </Table>
-                            </Col>
-                        </Row>
+                            </div>
+                        </Col>
+                        <Col xs={12}>
+                            <Table>
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>From</th>
+                                        <th>Message</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {props.stored.map((notification, index) => (
+                                        <MessageRow
+                                            key={index}
+                                            notification={notification}
+                                            onViewClicked={() => viewNotification(notification)}
+                                            onMarkReadClicked={() => markRead(notification)}
+                                            onMarkUnreadClicked={() => markUnread(notification)}
+                                        />
+                                    ))}
+                                </tbody>
+                            </Table>
+                        </Col>
+                    </Row>
 
-                    </CardBody>
-                </Card>
-            </>
-        );
-    }
-});
+                </CardBody>
+            </Card>
+        </>
+    );
+};
+
+export default connector(Messages);
