8095aba0e953bea6680d42ddd5718844d748d7cd
Same Old Nick <nick@sameoldnick.com>
2024-03-13 00:36:52 -0600

    Wrapped call to createIconFromSvgJson in React.useMemo

--- resources/ts/admin/components/icon-selector/Icon.tsx
+++ resources/ts/admin/components/icon-selector/Icon.tsx
@@ -12,11 +12,11 @@ interface IIconProps {
 const Icon: React.FC<IIconProps> = ({ icon, size }) => {
     const { svg: { tag, props, children } } = icon;
 
-    return createIconFromSvgJson({
+    return React.useMemo(() => createIconFromSvgJson({
         tag,
         props: { width: size, height: size, ...props },
         children
-    });
+    }), [icon, size]);
 }
 
 export default Icon;
