64b9f36c25c3255a3f0d72e6e53504a2a845c1dc
Same Old Nick <nick@sameoldnick.com>
2024-03-14 23:30:09 -0600

    Moved SingleComment component to separate file

--- resources/ts/admin/components/blog/comments/CommentList.tsx
+++ resources/ts/admin/components/blog/comments/CommentList.tsx
@@ -1,147 +1,25 @@
 import React from 'react';
-import { FaCheckCircle, FaEdit, FaSync, FaTimesCircle, FaToolbox, FaTrash } from 'react-icons/fa';
-import { Button, Col, Dropdown, DropdownItem, DropdownMenu, DropdownToggle, Form, Input, InputGroup, Row, Table } from 'reactstrap';
-import withReactContent from 'sweetalert2-react-content';
+import { FaSync } from 'react-icons/fa';
+import { Button, Col, Form, Input, InputGroup, Row, Table } from 'reactstrap';
 
 import { createAuthRequest } from '@admin/utils/api/factories';
 import Comment from '@admin/utils/api/models/Comment';
-import S from 'string';
-import { DateTime } from 'luxon';
-import axios from 'axios';
-import Swal from 'sweetalert2';
 
 import PaginatedTable from '@admin/components/PaginatedTable';
 import WaitToLoad, { IWaitToLoadHandle } from '@admin/components/WaitToLoad';
 import Loader from '@admin/components/Loader';
 
-import { CommentStatuses, approve, deny, loadAll } from '@admin/utils/api/endpoints/comments';
-import { defaultFormatter } from '@admin/utils/response-formatter/factories';
+import { CommentStatuses, loadAll } from '@admin/utils/api/endpoints/comments';
 
 import Article from '@admin/utils/api/models/Article';
 import User from '@admin/utils/api/models/User';
 import SelectArticleModal from '@admin/components/modals/SelectArticleModal';
 import SelectUserModal from '@admin/components/modals/SelectUserModal';
 import awaitModalPrompt from '@admin/utils/modals';
+import SingleComment from './SingleComment';
 
 interface IProps {
 
-}
-
-interface ICommentProps {
-    comment: Comment;
-    onUpdated: () => void;
-    setArticle: (article?: Article) => void;
-    setUser: (user?: User) => void;
-}
-
-const SingleComment: React.FC<ICommentProps> = ({ comment, onUpdated, setArticle, setUser }) => {
-    const [actionDropdown, setActionDropdown] = React.useState(false);
-
-    const handleApproveClicked = React.useCallback(async (e: React.MouseEvent) => {
-        e.preventDefault();
-
-        try {
-            if (!comment.comment.id)
-                throw new Error('Comment ID is missing.');
-
-            await approve(comment);
-
-            await withReactContent(Swal).fire({
-                icon: 'success',
-                title: 'Success!',
-                text: `The comment has been approved.`,
-                showConfirmButton: true,
-                showCancelButton: false
-            });
-
-            onUpdated();
-
-        } catch (err) {
-            console.error(err);
-
-            const message = defaultFormatter().parse(axios.isAxiosError(err) ? err.response : undefined);
-
-            await withReactContent(Swal).fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: `An error occurred approving comment: ${message}\nPlease try again.`,
-                showConfirmButton: false,
-                showCancelButton: true
-            });
-        }
-    }, [onUpdated]);
-
-    const handleDenyClicked = React.useCallback(async (e: React.MouseEvent) => {
-        e.preventDefault();
-
-        try {
-            if (!comment.comment.id)
-                throw new Error('Comment ID is missing.');
-
-            await deny(comment);
-
-            await withReactContent(Swal).fire({
-                icon: 'success',
-                title: 'Success!',
-                text: `The comment has been denied.`,
-                showConfirmButton: true,
-                showCancelButton: false
-            });
-
-            onUpdated();
-        } catch (err) {
-            console.error(err);
-
-            const message = defaultFormatter().parse(axios.isAxiosError(err) ? err.response : undefined);
-
-            await withReactContent(Swal).fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: `An error occurred denying comment: ${message}\nPlease try again.`,
-                showConfirmButton: false,
-                showCancelButton: true
-            });
-        }
-    }, [onUpdated]);
-
-    return (
-        <>
-            <tr>
-                <th scope='row'>{comment.comment.id}</th>
-                <td>
-                    <a href='#' title={comment.comment.article.title} onClick={() => setArticle(comment.article)}>
-                        {comment.comment.article_id}
-                    </a>
-                </td>
-                <td>
-                    <a href='#' onClick={() => setUser(comment.postedBy ?? undefined)}>
-                        {comment.postedBy?.user.email || comment.postedBy?.user.id}
-                    </a>
-                </td>
-
-                <td>{S(comment.comment.comment).truncate(100).s}</td>
-                <td>{comment.createdAt.toLocaleString(DateTime.DATETIME_SHORT)}</td>
-                <td>{S(comment.status).humanize().s}</td>
-                <td>
-                    <Dropdown toggle={() => setActionDropdown((prev) => !prev)} isOpen={actionDropdown}>
-                        <DropdownToggle caret color='primary'>
-                            <FaToolbox />{' '}
-                            Actions
-                        </DropdownToggle>
-                        <DropdownMenu>
-                            <DropdownItem href={comment.generatePath()}><FaEdit />{' '}Edit</DropdownItem>
-                            {comment.status !== Comment.STATUS_APPROVED && (
-                                <DropdownItem onClick={handleApproveClicked}><FaCheckCircle />{' '}Approve</DropdownItem>
-                            )}
-                            {comment.status !== Comment.STATUS_DENIED && (
-                                <DropdownItem onClick={handleDenyClicked}><FaTimesCircle />{' '}Deny</DropdownItem>
-                            )}
-                        </DropdownMenu>
-                    </Dropdown>
-                </td>
-            </tr>
-        </>
-    );
 }
 
 const CommentList: React.FC<IProps> = ({ }) => {
---
+++ resources/ts/admin/components/blog/comments/SingleComment.tsx
@@ -0,0 +1,135 @@
+import React from 'react';
+import { FaCheckCircle, FaEdit, FaTimesCircle, FaToolbox } from 'react-icons/fa';
+import { Dropdown, DropdownItem, DropdownMenu, DropdownToggle } from 'reactstrap';
+import withReactContent from 'sweetalert2-react-content';
+
+import Comment from '@admin/utils/api/models/Comment';
+import S from 'string';
+import { DateTime } from 'luxon';
+import axios from 'axios';
+import Swal from 'sweetalert2';
+
+import { approve, deny } from '@admin/utils/api/endpoints/comments';
+import { defaultFormatter } from '@admin/utils/response-formatter/factories';
+
+import Article from '@admin/utils/api/models/Article';
+import User from '@admin/utils/api/models/User';
+
+interface ICommentProps {
+    comment: Comment;
+    onUpdated: () => void;
+    setArticle: (article?: Article) => void;
+    setUser: (user?: User) => void;
+}
+
+const SingleComment: React.FC<ICommentProps> = ({ comment, onUpdated, setArticle, setUser }) => {
+    const [actionDropdown, setActionDropdown] = React.useState(false);
+
+    const handleApproveClicked = React.useCallback(async (e: React.MouseEvent) => {
+        e.preventDefault();
+
+        try {
+            if (!comment.comment.id)
+                throw new Error('Comment ID is missing.');
+
+            await approve(comment);
+
+            await withReactContent(Swal).fire({
+                icon: 'success',
+                title: 'Success!',
+                text: `The comment has been approved.`,
+                showConfirmButton: true,
+                showCancelButton: false
+            });
+
+            onUpdated();
+
+        } catch (err) {
+            console.error(err);
+
+            const message = defaultFormatter().parse(axios.isAxiosError(err) ? err.response : undefined);
+
+            await withReactContent(Swal).fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: `An error occurred approving comment: ${message}\nPlease try again.`,
+                showConfirmButton: false,
+                showCancelButton: true
+            });
+        }
+    }, [onUpdated]);
+
+    const handleDenyClicked = React.useCallback(async (e: React.MouseEvent) => {
+        e.preventDefault();
+
+        try {
+            if (!comment.comment.id)
+                throw new Error('Comment ID is missing.');
+
+            await deny(comment);
+
+            await withReactContent(Swal).fire({
+                icon: 'success',
+                title: 'Success!',
+                text: `The comment has been denied.`,
+                showConfirmButton: true,
+                showCancelButton: false
+            });
+
+            onUpdated();
+        } catch (err) {
+            console.error(err);
+
+            const message = defaultFormatter().parse(axios.isAxiosError(err) ? err.response : undefined);
+
+            await withReactContent(Swal).fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: `An error occurred denying comment: ${message}\nPlease try again.`,
+                showConfirmButton: false,
+                showCancelButton: true
+            });
+        }
+    }, [onUpdated]);
+
+    return (
+        <>
+            <tr>
+                <th scope='row'>{comment.comment.id}</th>
+                <td>
+                    <a href='#' title={comment.comment.article.title} onClick={() => setArticle(comment.article)}>
+                        {comment.comment.article_id}
+                    </a>
+                </td>
+                <td>
+                    <a href='#' onClick={() => setUser(comment.postedBy ?? undefined)}>
+                        {comment.postedBy?.user.email || comment.postedBy?.user.id}
+                    </a>
+                </td>
+
+                <td>{S(comment.comment.comment).truncate(100).s}</td>
+                <td>{comment.createdAt.toLocaleString(DateTime.DATETIME_SHORT)}</td>
+                <td>{S(comment.status).humanize().s}</td>
+                <td>
+                    <Dropdown toggle={() => setActionDropdown((prev) => !prev)} isOpen={actionDropdown}>
+                        <DropdownToggle caret color='primary'>
+                            <FaToolbox />{' '}
+                            Actions
+                        </DropdownToggle>
+                        <DropdownMenu>
+                            <DropdownItem href={comment.generatePath()}><FaEdit />{' '}Edit</DropdownItem>
+                            {comment.status !== Comment.STATUS_APPROVED && (
+                                <DropdownItem onClick={handleApproveClicked}><FaCheckCircle />{' '}Approve</DropdownItem>
+                            )}
+                            {comment.status !== Comment.STATUS_DENIED && (
+                                <DropdownItem onClick={handleDenyClicked}><FaTimesCircle />{' '}Deny</DropdownItem>
+                            )}
+                        </DropdownMenu>
+                    </Dropdown>
+                </td>
+            </tr>
+        </>
+    );
+}
+
+export default SingleComment;
