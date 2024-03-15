3035fa3fe1d427bb80f56d1a57c0080677cb41d4
Same Old Nick <nick@sameoldnick.com>
2024-03-15 00:21:11 -0600

    Displays "Coming Soon" message when search form is submitted

--- resources/ts/admin/layouts/admin/topbar/SearchForm.tsx
+++ resources/ts/admin/layouts/admin/topbar/SearchForm.tsx
@@ -3,20 +3,32 @@ import { FaSearch } from 'react-icons/fa';
 import { Form, InputGroup, Input, Button, FormProps } from 'reactstrap';
 
 import classNames from 'classnames';
+import withReactContent from 'sweetalert2-react-content';
+import Swal from 'sweetalert2';
 
-interface IProps extends FormProps {
+interface IProps extends Omit<FormProps, 'onSubmit'> {
 
 }
 
 const SearchForm: React.FC<IProps> = ({ className, ...props }) => {
+    const handleSubmit = React.useCallback((e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        withReactContent(Swal).fire({
+            icon: 'info',
+            title: 'Coming Soon!',
+            text: 'The search feature for the dashboard has not been implemented yet.'
+        });
+    }, []);
+
     return (
         <>
-            <Form className={classNames("navbar-search", className)} {...props}>
+            <Form className={classNames("navbar-search", className)} onSubmit={handleSubmit} {...props}>
                 <InputGroup>
                     <Input type="search" className="bg-light border-0 small" placeholder="Search for..." aria-label="Search" />
                     {/* The InputGroupText component does not use the input-group-append class */}
                     <div className='input-group-append'>
-                        <Button color='primary' type='button'>
+                        <Button color='primary' type='submit'>
                             <FaSearch className='fa-sm' />
                         </Button>
                     </div>
