993de134247d14d2f5c7167a540fb23998ad4450
Same Old Nick <nick@sameoldnick.com>
2024-03-16 23:46:03 -0600

    Returns notified users

--- app/Traits/Support/NotifiesRoles.php
+++ app/Traits/Support/NotifiesRoles.php
@@ -8,10 +8,19 @@ use Illuminate\Support\Facades\Notification;
 
 trait NotifiesRoles
 {
+    /**
+     * Sends notification to users with roles.
+     *
+     * @param array $roles The roles the users must have
+     * @param mixed $notification
+     * @return array Users that were notified
+     */
     public function notifyRoles($roles, $notification)
     {
         $users = User::getUsersWithRoles(Arr::wrap($roles))->all();
 
         Notification::send($users, $notification);
+
+        return $users;
     }
 }
