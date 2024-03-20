a36eb69d89f2ac8fe7e31849736a71db0b229ffc
Same Old Nick <nick@sameoldnick.com>
2024-03-20 00:36:43 -0600

    Added factory for SocialMedia model

---
+++ database/factories/SocialMediaLinkFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\SocialMediaLinkFactory>
+ */
+class SocialMediaLinkFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'link' => $this->faker->unique()->socialMediaLink(),
+        ];
+    }
+}
