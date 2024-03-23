b6c2c6ff1afb5462e230c77757cd00bbce94fb3f
Same Old Nick <nick@sameoldnick.com>
2024-03-22 23:32:21 -0600

    Moved Article to separate ArticleRow component

--- resources/ts/admin/components/blog/article-list/ArticleList.tsx
+++ resources/ts/admin/components/blog/article-list/ArticleList.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { FaCalendarAlt, FaEdit, FaExternalLinkAlt, FaFileAlt, FaPlus, FaSave, FaSync, FaToolbox, FaTrash, FaUndo } from 'react-icons/fa';
-import { Button, Col, Dropdown, DropdownItem, DropdownMenu, DropdownToggle, Form, Input, Row, Table } from 'reactstrap';
-import withReactContent from 'sweetalert2-react-content';
+import { FaPlus, FaSync } from 'react-icons/fa';
+import { Button, Col, Form, Input, Row, Table } from 'reactstrap';
 
-import S from 'string';
-import axios from 'axios';
-import Swal from 'sweetalert2';
-import { DateTime } from 'luxon';
-
-import SelectDateTimeModal from '@admin/components/modals/SelectDateTimeModal';
 import PaginatedTable from '@admin/components/PaginatedTable';
 import Loader from '@admin/components/Loader';
 import WaitToLoad, { IWaitToLoadHandle } from '@admin/components/WaitToLoad';
+import ArticleRow from './ArticleRow';
 
 import Article from '@admin/utils/api/models/Article';
 
 import { createAuthRequest } from '@admin/utils/api/factories';
-import { updateArticle, restoreArticle as restoreArticleApi, deleteArticle as deleteArticleApi, fetchArticles, ArticleStatuses } from '@admin/utils/api/endpoints/articles';
-import { defaultFormatter } from '@admin/utils/response-formatter/factories';
-import awaitModalPrompt from '@admin/utils/modals';
+import { fetchArticles, ArticleStatuses } from '@admin/utils/api/endpoints/articles';
 
 interface IProps {
 
@@ -29,271 +21,9 @@ interface IState {
     show: ArticleStatuses;
 }
 
-interface IArticleProps {
-    article: Article;
-    onUpdated: () => void;
-}
+
 
 export default class ArticleList extends React.Component<IProps, IState> {
-    static Article: React.FC<IArticleProps> = ({ article, onUpdated }) => {
-        const [actionDropdown, setActionDropdown] = React.useState(false);
-
-        const handlePublishClicked = React.useCallback(async () => {
-            const result = await withReactContent(Swal).fire({
-                icon: 'question',
-                title: 'Are You Sure?',
-                text: `The article will become visible to all users.`,
-                showConfirmButton: true,
-                showCancelButton: true
-            });
-
-            if (result.isConfirmed) {
-                await publishArticle();
-
-                onUpdated();
-            }
-        }, [onUpdated]);
-
-        const publishArticle = React.useCallback(async () => {
-            try {
-                await updateArticle(article.article.id, article.article.title, article.article.slug, DateTime.now());
-            } catch (err) {
-                console.error(err);
-
-                const message = defaultFormatter().parse(axios.isAxiosError(err) ? err.response : undefined);
-
-                const result = await withReactContent(Swal).fire({
-                    icon: 'error',
-                    title: 'Ooops...',
-                    text: `An error occurred: ${message}`,
-                    showConfirmButton: true,
-                    confirmButtonText: 'Try Again',
-                    showCancelButton: true
-                });
-
-                if (result.isConfirmed) {
-                    await publishArticle();
-                }
-            }
-        }, []);
-
-        const handleScheduleClicked = React.useCallback(async () => {
-            try {
-                const dateTime = await awaitModalPrompt(SelectDateTimeModal);
-
-                scheduleArticle(dateTime);
-            } catch (err) {
-                // User cancelled modal.
-            }
-        }, []);
-
-        const scheduleArticle = React.useCallback(async (dateTime: DateTime) => {
-            try {
-                await updateArticle(article.article.id, article.article.title, article.article.slug, dateTime);
-
-                onUpdated();
-            } catch (err) {
-                console.error(err);
-
-                const message = defaultFormatter().parse(axios.isAxiosError(err) ? err.response : undefined);
-
-                const result = await withReactContent(Swal).fire({
-                    icon: 'error',
-                    title: 'Ooops...',
-                    text: `An error occurred: ${message}`,
-                    showConfirmButton: true,
-                    confirmButtonText: 'Try Again',
-                    showCancelButton: true
-                });
-
-                if (result.isConfirmed) {
-                    await scheduleArticle(dateTime);
-                }
-            }
-        }, [onUpdated]);
-
-        const handleUnpublishClicked = React.useCallback(async () => {
-            const result = await withReactContent(Swal).fire({
-                icon: 'question',
-                title: 'Are You Sure?',
-                text: `The article will become hidden.`,
-                showConfirmButton: true,
-                showCancelButton: true
-            });
-
-            if (result.isConfirmed) {
-                await unpublishArticle();
-                onUpdated();
-            }
-        }, [onUpdated]);
-
-        const unpublishArticle = React.useCallback(async () => {
-            try {
-                await updateArticle(article.article.id, article.article.title, article.article.slug, null);
-            } catch (err) {
-                console.error(err);
-
-                const message = defaultFormatter().parse(axios.isAxiosError(err) ? err.response : undefined);
-
-                const result = await withReactContent(Swal).fire({
-                    icon: 'error',
-                    title: 'Ooops...',
-                    text: `An error occurred: ${message}`,
-                    showConfirmButton: true,
-                    confirmButtonText: 'Try Again',
-                    showCancelButton: true
-                });
-
-                if (result.isConfirmed) {
-                    await unpublishArticle();
-                }
-            }
-        }, []);
-
-        const handleDeleteClicked = React.useCallback(async () => {
-            const result = await withReactContent(Swal).fire({
-                icon: 'question',
-                title: 'Are You Sure?',
-                text: `The article can be restored later, if needed.`,
-                showConfirmButton: true,
-                showCancelButton: true
-            });
-
-            if (result.isConfirmed) {
-                await deleteArticle();
-                onUpdated();
-            }
-        }, [onUpdated]);
-
-        const deleteArticle = React.useCallback(async () => {
-            try {
-                await deleteArticleApi(article.article.id);
-
-                onUpdated();
-            } catch (err) {
-                console.error(err);
-
-                const message = defaultFormatter().parse(axios.isAxiosError(err) ? err.response : undefined);
-
-                const result = await withReactContent(Swal).fire({
-                    icon: 'error',
-                    title: 'Ooops...',
-                    text: `An error occurred: ${message}`,
-                    showConfirmButton: true,
-                    confirmButtonText: 'Try Again',
-                    showCancelButton: true
-                });
-
-                if (result.isConfirmed) {
-                    await unpublishArticle();
-                }
-            }
-        }, [onUpdated]);
-
-        const handleRestoreClicked = React.useCallback(async () => {
-            const result = await withReactContent(Swal).fire({
-                icon: 'question',
-                title: 'Are You Sure?',
-                text: `The article maybe made public.`,
-                showConfirmButton: true,
-                showCancelButton: true
-            });
-
-            if (result.isConfirmed) {
-                await restoreArticle();
-                onUpdated();
-            }
-        }, [onUpdated]);
-
-        const restoreArticle = React.useCallback(async () => {
-            try {
-                await restoreArticleApi(article.article.id);
-
-                onUpdated();
-            } catch (err) {
-                console.error(err);
-
-                const message = defaultFormatter().parse(axios.isAxiosError(err) ? err.response : undefined);
-
-                const result = await withReactContent(Swal).fire({
-                    icon: 'error',
-                    title: 'Ooops...',
-                    text: `An error occurred: ${message}`,
-                    showConfirmButton: true,
-                    confirmButtonText: 'Try Again',
-                    showCancelButton: true
-                });
-
-                if (result.isConfirmed) {
-                    await restoreArticle();
-                }
-            }
-        }, [onUpdated]);
-
-        return (
-            <>
-                <tr>
-                    <td>{article.article.id}</td>
-                    <td>{article.article.title}</td>
-                    <td>{S(article.article.revision?.summary).truncate(75).s}</td>
-                    <td>{S(article.status).capitalize().s}</td>
-                    <td>
-                        {article.status === Article.ARTICLE_STATUS_UNPUBLISHED &&
-                            (
-                                <>
-                                    <Dropdown group toggle={() => setActionDropdown((prev) => !prev)} isOpen={actionDropdown}>
-                                        <DropdownToggle caret color='primary'>
-                                            <FaToolbox />{' '}
-                                            Actions
-                                        </DropdownToggle>
-                                        <DropdownMenu>
-                                            <DropdownItem href={article.article.private_url} target='_blank'><FaExternalLinkAlt />{' '}Preview</DropdownItem>
-                                            <DropdownItem href={`posts/edit/${article.article.id}`}><FaEdit />{' '}Edit</DropdownItem>
-                                            <DropdownItem onClick={handlePublishClicked}><FaSave />{' '}Publish Now</DropdownItem>
-                                            <DropdownItem onClick={handleScheduleClicked}><FaCalendarAlt />{' '}Schedule</DropdownItem>
-                                            <DropdownItem onClick={handleDeleteClicked}><FaTrash />{' '}Delete</DropdownItem>
-                                        </DropdownMenu>
-                                    </Dropdown>
-                                </>
-                            )
-                        }
-
-                        {(article.status === Article.ARTICLE_STATUS_PUBLISHED || article.status === Article.ARTICLE_STATUS_SCHEDULED) &&
-                            (
-                                <>
-                                    <Dropdown group toggle={() => setActionDropdown((prev) => !prev)} isOpen={actionDropdown}>
-                                        <DropdownToggle caret color='primary'>
-                                            <FaToolbox />{' '}
-                                            Actions
-                                        </DropdownToggle>
-                                        <DropdownMenu>
-                                            <DropdownItem href={article.article.private_url} target='_blank'><FaExternalLinkAlt />{' '}Preview</DropdownItem>
-                                            <DropdownItem href={`posts/edit/${article.article.id}`}><FaEdit />{' '}Edit</DropdownItem>
-                                            <DropdownItem onClick={handleUnpublishClicked}><FaFileAlt />{' '}Unpublish</DropdownItem>
-                                            <DropdownItem onClick={handleDeleteClicked}><FaTrash />{' '}Delete</DropdownItem>
-                                        </DropdownMenu>
-                                    </Dropdown>
-                                </>
-                            )
-                        }
-
-                        {article.status === Article.ARTICLE_STATUS_DELETED &&
-                            (
-                                <>
-                                    <Button color='primary' onClick={handleRestoreClicked} title='Undelete' className='me-1'>
-                                        <FaUndo />{' '}
-                                        Restore
-                                    </Button>
-                                </>
-                            )
-                        }
-
-                    </td>
-                </tr>
-            </>
-        );
-    }
-
     private readonly _waitToLoadArticlesRef = React.createRef<IWaitToLoadHandle>();
     private readonly _paginatedTableRef = React.createRef<PaginatedTable<IArticle>>();
 
@@ -399,7 +129,7 @@ export default class ArticleList extends React.Component<IProps, IState> {
                                                             </tr>
                                                         )}
                                                         {data.length > 0 && data.map((article, index) =>
-                                                            <ArticleList.Article
+                                                            <ArticleRow
                                                                 key={index}
                                                                 article={new Article(article)}
                                                                 onUpdated={this.loadArticles}
