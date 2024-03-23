0d01e308a60865f84a18891d45506aac1ca2ca5a
Same Old Nick <nick@sameoldnick.com>
2024-03-22 22:26:49 -0600

    WithRoles traits allows roles to be included with tests

---
+++ tests/Feature/Traits/WithRoles.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature\Traits;
+
+use Illuminate\Support\Arr;
+
+trait WithRoles
+{
+    use CreatesUser;
+    use InteractsWithJWT;
+
+    /**
+     * Gets all possible roles.
+     *
+     * @return array
+     */
+    protected function possibleRoles(): array {
+        return Arr::map(config('roles.roles'), fn ($role) => $role['id']);
+    }
+
+    /**
+     * Attaches user with roles for making HTTP requests.
+     *
+     * @param array $roles
+     * @return static
+     */
+    protected function withRoles(array $roles): static {
+        $user = $this->createUser($roles);
+
+        return $this->actingAs($user);
+    }
+}
