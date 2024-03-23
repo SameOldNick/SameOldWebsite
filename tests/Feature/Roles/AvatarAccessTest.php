3fa41506e2aac1ff549ebb36b30c54b0934cd290
Same Old Nick <nick@sameoldnick.com>
2024-03-22 23:08:02 -0600

    Tests users are authorized to change avatar

---
+++ tests/Feature/Roles/AvatarAccessTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\Feature\Traits\DisablesVite;
+use Tests\Feature\Traits\WithRoles;
+use Tests\TestCase;
+
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+
+class AvatarAccessTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithRoles;
+    use WithFaker;
+    use DisablesVite;
+
+    /**
+     * Tests user is authorized to upload avatar.
+     *
+     * @return void
+     */
+    public function testCanUploadAvatar(): void
+    {
+        Storage::fake('avatars');
+
+        $file = UploadedFile::fake()->image('avatar.jpg');
+
+        $response = $this->withRoles(['change_avatar'])->postJson('/api/user/avatar', [
+            'avatar' => $file
+        ]);
+
+        $response->assertSuccessful();
+    }
+
+    /**
+     * Tests user is unauthorized to upload avatar.
+     *
+     * @return void
+     */
+    public function testCannotUploadAvatar(): void
+    {
+        Storage::fake('avatars');
+
+        $file = UploadedFile::fake()->image('avatar.jpg');
+
+        $response = $this->withRoles([])->postJson('/api/user/avatar', [
+            'avatar' => $file
+        ]);
+
+        $response->assertForbidden();
+    }
+
+    /**
+     * Tests user is authorized to delete avatar.
+     *
+     * @return void
+     */
+    public function testCanDeleteAvatar(): void
+    {
+        $response = $this->withRoles(['change_avatar'])->deleteJson('/api/user/avatar');
+
+        $response->assertNotFound();
+    }
+
+    /**
+     * Tests user is unauthorized to delete avatar.
+     *
+     * @return void
+     */
+    public function testCannotDeleteAvatar(): void
+    {
+        $response = $this->withRoles([])->deleteJson('/api/user/avatar');
+
+        $response->assertForbidden();
+    }
+
+
+}
