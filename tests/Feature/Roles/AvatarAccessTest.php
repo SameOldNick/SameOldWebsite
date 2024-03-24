0acb6588fa23838cf1e98c28fce812a8ac87d82f
Same Old Nick <nick@sameoldnick.com>
2024-03-24 12:27:31 -0600

    Fixed namespace

--- tests/Feature/Roles/AvatarAccessTest.php
+++ tests/Feature/Roles/AvatarAccessTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature\Http\Controllers\Api;
+namespace Tests\Feature\Roles;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
--- tests/Feature/Roles/CommentsAccessTest.php
+++ tests/Feature/Roles/CommentsAccessTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature\Http\Controllers\Api;
+namespace Tests\Feature\Roles;
 
 use App\Models\Article;
 use App\Models\Comment;
