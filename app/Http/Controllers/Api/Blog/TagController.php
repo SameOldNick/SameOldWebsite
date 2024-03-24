175ea607e96c6256dfd76fd46371feea5d4e7fa2
Same Old Nick <nick@sameoldnick.com>
2024-03-23 18:32:04 -0600

    Only users with write_posts role can associate tags with articles

--- app/Http/Controllers/Api/Blog/TagController.php
+++ app/Http/Controllers/Api/Blog/TagController.php
@@ -9,6 +9,11 @@ use Illuminate\Http\Request;
 
 class TagController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('can:role-write-posts');
+    }
+
     /**
      * Display a listing of the resource.
      */
