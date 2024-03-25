b24f6cb844d942361a54f20aab26d27c176ec8fb
Same Old Nick <nick@sameoldnick.com>
2024-03-24 23:27:51 -0600

    Added ContactMessage factory

---
+++ database/factories/ContactMessageFactory.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ContactMessage>
+ */
+class ContactMessageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'email' => $this->faker->email,
+            'message' => $this->faker->realText,
+        ];
+    }
+
+    /**
+     * Marks the contact message as confirmed.
+     *
+     * @return static
+     */
+    public function confirmed() {
+        return $this->state([
+            'approved_at' => $this->faker->dateTimeBetween('now', '+1 month')
+        ]);
+    }
+
+    /**
+     * Marks the contact message as requiring confirmation.
+     *
+     * @return static
+     */
+    public function requiresConfirmation() {
+        return $this->state([
+            'expires_at' => $this->faker->dateTimeBetween('-1 month', '+1 month')
+        ]);
+    }
+}
