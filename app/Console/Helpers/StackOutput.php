efe4fe7099995fbd5e0288d76814b7c7a977b617
Same Old Nick <nick@sameoldnick.com>
2024-04-26 00:49:44 -0600

    Allows output to be sent through multiple channels

---
+++ app/Console/Helpers/StackOutput.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Helpers;
+
+use Symfony\Component\Console\Output\Output;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class StackOutput extends Output
+{
+    /**
+     * Initializes StackOutput instance
+     *
+     * @param OutputInterface[] $outputs
+     */
+    public function __construct(
+        protected readonly array $outputs
+    )
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Writes a message to the output.
+     *
+     * @return void
+     */
+    protected function doWrite(string $message, bool $newline) {
+        foreach ($this->outputs as $output) {
+            $output->write($message, $newline);
+        }
+    }
+}
