3f53a52ffcd3823b41b8b8266b696b0d89d1c671
Same Old Nick <nick@sameoldnick.com>
2024-04-27 17:14:13 -0600

    Create CreatesSeeders.php

---
+++ app/Traits/Console/CreatesSeeders.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Traits\Console;
+
+trait CreatesSeeders {
+    /**
+     * Creates Seeder instance
+     *
+     * @param class-string<\Illuminate\Database\Seeder> $class
+     * @return \Illuminate\Database\Seeder
+     */
+    protected function createSeeder(string $class) {
+        return
+            $this->getLaravel()->make($class)
+                ->setContainer($this->getLaravel())
+                ->setCommand($this);
+    }
+}
