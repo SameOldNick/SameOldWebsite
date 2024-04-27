e9ce1251e96d01b1f21ef1169ef56a0965866707
SameOldNick <SameOldNick@users.noreply.github.com>
2024-04-27 23:54:57 +0000

    Applied php-cs-fixer changes

--- app/Traits/Console/CreatesSeeders.php
+++ app/Traits/Console/CreatesSeeders.php
@@ -2,14 +2,16 @@
 
 namespace App\Traits\Console;
 
-trait CreatesSeeders {
+trait CreatesSeeders
+{
     /**
      * Creates Seeder instance
      *
      * @param class-string<\Illuminate\Database\Seeder> $class
      * @return \Illuminate\Database\Seeder
      */
-    protected function createSeeder(string $class) {
+    protected function createSeeder(string $class)
+    {
         return
             $this->getLaravel()->make($class)
                 ->setContainer($this->getLaravel())
