7cb714c3d425076febb86116e82e7ee18cc765c1
Same Old Nick <nick@sameoldnick.com>
2024-04-28 00:14:51 -0600

    Assumes user has access

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -13,7 +13,7 @@ class UpdateUserRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return ! is_null($this->user()) && $this->user()->hasAllRoles(['admin']);
+        return true;
     }
 
     /**
