979b6138942b49418bd20d6eb37013eeaa4eaad4
Same Old Nick <nick@sameoldnick.com>
2024-04-27 22:03:07 -0600

    Sets up the database

---
+++ app/Console/Commands/Setup/SetupDatabase.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Console\Commands\Setup;
+
+use Illuminate\Console\Command;
+
+class SetupDatabase extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'setup:database
+                            {--y|yes : Proceed without asking for confirmation}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Setup the database including migrations and seeding';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        // Extract options
+        $skipPrompt = $this->option('yes');
+
+        // Function to confirm database setup
+        $this->confirmDatabaseSetup($skipPrompt);
+
+        // Run migrations
+        $this->info("Running database migrations...");
+        $this->call('migrate:fresh', []);
+
+        // Seed the database
+        $this->info("Seeding the database...");
+        $this->call('db:seed', ['--class' => 'SetupSeeder']);
+
+        $this->info("Database setup complete.");
+    }
+
+    /**
+     * Confirms database setup
+     *
+     * @param boolean $skipPrompt
+     * @return void
+     */
+    private function confirmDatabaseSetup(bool $skipPrompt)
+    {
+        if (!$skipPrompt) {
+            if (!$this->confirm("This will erase everything in the database. Are you sure you want to continue?")) {
+                $this->error("Cancelled database setup.");
+                exit(1);
+            }
+        }
+    }
+}
