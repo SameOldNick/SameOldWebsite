34974b6b048583002fdf2853ae00523e67bbc9b6
Same Old Nick <nick@sameoldnick.com>
2024-04-27 21:57:21 -0600

    Existing user can be passed to InitialSeeder

--- database/seeders/InitialSeeder.php
+++ database/seeders/InitialSeeder.php
@@ -3,20 +3,41 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Str;
 
 class InitialSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(User $user = null): void
     {
+        // Laravel might try to dig up a non-existent model, so also check if it exists.
+        if (is_null($user) || !$user->exists) {
+            $user = $this->createUser();
+        }
+
         $this->call([
             Initial\ProjectSeeder::class,
             Initial\SkillSeeder::class,
             Initial\TechnologySeeder::class,
-            Initial\UserSeeder::class,
-            Initial\ArticleSeeder::class,
         ]);
+
+        $this->callWith(Initial\RoleUserSeeder::class, ['user' => $user]);
+        $this->callWith(Initial\ArticleSeeder::class, ['user' => $user]);
+    }
+
+    /**
+     * Create user
+     *
+     * @return User
+     */
+    protected function createUser() {
+        $uuid = (string) Str::uuid();
+
+        $this->callWith(Initial\UserSeeder::class, ['additional' => ['uuid' => $uuid]]);
+
+        return User::firstWhere('uuid', $uuid);
     }
 }
