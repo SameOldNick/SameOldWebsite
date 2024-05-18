f8724aef3b713e9ec98f56967fdaa437e6045e9a
Same Old Nick <nick@sameoldnick.com>
2024-05-18 00:51:47 -0600

    Uses Laravel 11 skeleton for building the app

---
+++ bootstrap/providers.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\AuthServiceProvider::class,
+    // App\Providers\BroadcastServiceProvider::class,
+    App\Providers\EventServiceProvider::class,
+    App\Providers\RouteServiceProvider::class,
+    App\Providers\ComponentServiceProvider::class,
+    App\Providers\ValidationServiceProvider::class,
+    App\Providers\ViewServiceProvider::class,
+    App\Providers\MenuServiceProvider::class,
+];
--- config/app.php
+++ config/app.php
@@ -181,23 +181,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-        App\Providers\ComponentServiceProvider::class,
-        App\Providers\ValidationServiceProvider::class,
-        App\Providers\ViewServiceProvider::class,
-        App\Providers\MenuServiceProvider::class,
     ],
 
     /*
