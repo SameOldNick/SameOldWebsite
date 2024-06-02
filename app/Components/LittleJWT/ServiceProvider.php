24f33380f304c8467ee9a185dc687bd49db0e3e3
Same Old Nick <nick@sameoldnick.com>
2024-06-02 00:26:09 -0600

    Updated LittleJWT to v2.2.1

--- app/Components/LittleJWT/ServiceProvider.php
+++ app/Components/LittleJWT/ServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Components\LittleJWT;
 
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 use LittleApps\LittleJWT\Contracts\Keyable;
+use LittleApps\LittleJWT\Factories\KeyBuilder;
 use LittleApps\LittleJWT\LittleJWT;
 
 class ServiceProvider extends BaseServiceProvider
@@ -16,8 +17,11 @@ class ServiceProvider extends BaseServiceProvider
     public function register()
     {
         $this->app->singleton('littlejwt.refresh', function ($app) {
-            $jwk = $app->make(Keyable::class)->buildFromSecret([
-                'phrase' => env('LITTLEJWT_KEY_PHRASE_REFRESH', ''),
+            $jwk = KeyBuilder::buildFromConfig([
+                'default' => KeyBuilder::KEY_SECRET,
+                KeyBuilder::KEY_SECRET => [
+                    'phrase' => env('LITTLEJWT_KEY_PHRASE_REFRESH', ''),
+                ]
             ]);
 
             return new LittleJWT($app, $jwk);
