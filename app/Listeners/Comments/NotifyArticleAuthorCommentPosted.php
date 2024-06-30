2e3fb609dd6e956182b3d355e9e0aa2bf2c07c36
Same Old Nick <nick@sameoldnick.com>
2024-06-29 23:35:22 -0600

    Added check that article has author

--- app/Listeners/Comments/NotifyArticleAuthorCommentPosted.php
+++ app/Listeners/Comments/NotifyArticleAuthorCommentPosted.php
@@ -4,6 +4,7 @@ namespace App\Listeners\Comments;
 
 use App\Events\Comments\CommentApproved;
 use App\Notifications\CommentPosted;
+use Illuminate\Support\Facades\Log;
 
 class NotifyArticleAuthorCommentPosted
 {
@@ -21,6 +22,15 @@ class NotifyArticleAuthorCommentPosted
     public function handle(CommentApproved $event): void
     {
         $author = $event->comment->article->post->user;
+
+        /**
+         * The article should have a user.
+         * This check is for when the article wasn't created correctly in testing.
+         */
+        if (is_null($author)) {
+            Log::error('Unable to determine article author to send notification', ['article' => $event->comment->article]);
+            return;
+        }
 
         $author->notify(new CommentPosted($event->comment));
     }
