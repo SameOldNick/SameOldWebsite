517c59ce79382d10efced2448d15f8c737019a79
Same Old Nick <nick@sameoldnick.com>
2024-07-30 20:04:18 -0600

    Fixed "Using emergency logger." error messages

--- app/Components/Security/Integrations/ServiceProvider.php
+++ app/Components/Security/Integrations/ServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Components\Security\Integrations;
 
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use Monolog\Logger as Monolog;
 
 final class ServiceProvider extends BaseServiceProvider
 {
@@ -11,12 +12,26 @@ final class ServiceProvider extends BaseServiceProvider
      *
      * @return void
      */
-    public function register() {}
+    public function register() {
+        /**
+         * The log driver needs to be registered immediately.
+         * Registering the driver later will cause it to be not found and the log will be
+         * filled with "Unable to create configured logger. Using emergency logger." messages.
+         */
+        $this->app['log']->extend('sentinel', function ($app, $config) {
+            $level = $config['level'] ?? 'debug';
+            $handler = new LogHandler($level);
+
+            return new Monolog('sentinel', [$handler]);
+        });
+    }
 
     /**
      * Bootstrap any application services.
      *
      * @return void
      */
-    public function boot() {}
+    public function boot()
+    {
+    }
 }
