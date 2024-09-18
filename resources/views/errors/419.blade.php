987ffc7a67c1ff428282c9e1a8e033a4d7d42a42
Same Old Nick <nick@sameoldnick.com>
2024-09-18 17:35:07 -0600

    Added information about request coming from HTTP instead of HTTPS

--- resources/views/errors/419.blade.php
+++ resources/views/errors/419.blade.php
@@ -5,5 +5,12 @@
 @section('message', __('The page you were trying to access has expired.'))
 @section('content')
     <div class="text-center">
+        <p>{{ __('This may be caused by sending a request to the wrong URL scheme.') }}</p>
+        <p>
+            {{ __('Please ensure you\'re accessing :https instead of :http.', [
+                'https' => str_replace('http://', 'https://', url()->previous()),
+                'http' => str_replace('https://', 'http://', url()->previous())
+            ]) }}
+        </p>
     </div>
 @endsection
