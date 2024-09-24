50d133d3d271be7c37d1b94a82dac3a0128f5b04
Nick Hamnett <nick@sameoldnick.com>
2024-09-24 00:23:40 -0600

    Cleaned up typings

--- resources/ts/admin/typings/notifications.d.ts
+++ resources/ts/admin/typings/notifications.d.ts
@@ -9,20 +9,6 @@ declare global {
         updated_at: string;
         read_at: string | null;
     }
-
-    interface IAddress {
-        name: string | null;
-        address: string;
-    }
-
-    interface IMessageData {
-        addresses: Record<'to' | 'cc' | 'bcc' | 'replyTo', IAddress[]>;
-        subject: string;
-        view: Record<'html' | 'text', string>;
-        type: string;
-    }
-
-    type TMessageNotification = INotification<'6414fd8c-847a-492b-a919-a5fc539456e8', IMessageData>;
 }
 
 export { };
