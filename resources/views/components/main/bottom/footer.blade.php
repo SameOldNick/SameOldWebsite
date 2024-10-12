1bb56dad15332bc6f3a1d5112680f8a261384e66
Same Old Nick <nick@sameoldnick.com>
2024-10-12 16:13:23 -0600

    Removed empty anchor tag

--- resources/views/components/main/bottom/footer.blade.php
+++ resources/views/components/main/bottom/footer.blade.php
@@ -7,10 +7,6 @@
                 </a>
             </p>
 
-            <a href="/" class="col-md-2 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-                <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-            </a>
-
             <x-menu name="footer" renderer="footer" />
         </div>
     </div>
