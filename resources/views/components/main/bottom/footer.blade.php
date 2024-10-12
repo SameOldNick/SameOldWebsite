2c4a556e829220504caa004606fb23803c33b2b1
Same Old Nick <nick@sameoldnick.com>
2024-10-12 16:12:41 -0600

    Fixes horizontal scroll in mobile view

--- resources/views/components/main/bottom/footer.blade.php
+++ resources/views/components/main/bottom/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="row flex-wrap justify-content-between align-items-center">
             <p class="col-md-4 mb-0 brand">
                 <a href="{{ url('/') }}">
-                    <img src="{{ Vite::asset('resources/images/sameoldnick-text.png') }}" alt="{{ __('Same Old Nick') }}">
+                    <img src="{{ Vite::asset('resources/images/sameoldnick-text.png') }}" alt="{{ __('Same Old Nick') }}" class="img-fluid">
                 </a>
             </p>
 
