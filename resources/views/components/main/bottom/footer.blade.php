e6976f6130e6619b6478f2b151a31ec1148498ed
Same Old Nick <nick@sameoldnick.com>
2024-10-12 20:27:36 -0600

    Centers logo in footer

--- resources/views/components/main/bottom/footer.blade.php
+++ resources/views/components/main/bottom/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="bg-secondary py-5">
     <div class="container">
         <div class="row flex-wrap justify-content-between align-items-center">
-            <p class="col-md-4 mb-0 brand">
+            <p class="col-md-4 mb-0 brand text-center">
                 <a href="{{ url('/') }}">
                     <img src="{{ Vite::asset('resources/images/sameoldnick-text.png') }}" alt="{{ __('Same Old Nick') }}" class="img-fluid">
                 </a>
