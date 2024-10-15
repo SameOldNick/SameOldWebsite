aae691532fca88be496cccc04f944d3972ba465f
Same Old Nick <nick@sameoldnick.com>
2024-10-14 23:00:56 -0600

    Removed messages slice

--- resources/ts/admin/store/reducers.ts
+++ resources/ts/admin/store/reducers.ts
@@ -5,14 +5,12 @@ import accountSlice from './slices/account';
 import alertsSlice from './slices/alerts';
 import mainSlice from './slices/main';
 import notificationsSlice from './slices/notifications';
-import messagesSlice from './slices/messages';
 
 const reducers = combineReducers({
     main: mainSlice.reducer,
     account: accountSlice.reducer,
     alerts: alertsSlice.reducer,
     notifications: notificationsSlice.reducer,
-    messages: messagesSlice.reducer,
 });
 
 export default reducers;
