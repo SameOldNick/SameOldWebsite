aca44ef1ffda51516bf63876c22e0850ff1ef903
Same Old Nick <nick@sameoldnick.com>
2024-10-15 00:29:23 -0600

    A callback can be passed for the notification

--- app/Traits/Support/NotifiesRoles.php
+++ app/Traits/Support/NotifiesRoles.php
@@ -12,14 +12,20 @@ trait NotifiesRoles
      * Sends notification to users with roles.
      *
      * @param  array  $roles  The roles the users must have
-     * @param  mixed  $notification
+     * @param  mixed  $notification The notification or callback that is invoked with the User instance.
      * @return User[] Users that were notified
      */
     public function notifyRoles($roles, $notification)
     {
         $users = User::getUsersWithRoles(Arr::wrap($roles))->all();
 
-        Notification::send($users, $notification);
+        if (is_callable($notification)) {
+            foreach ($users as $user) {
+                Notification::send($user, $notification($user));
+            }
+        } else {
+            Notification::send($users, $notification);
+        }
 
         return $users;
     }
