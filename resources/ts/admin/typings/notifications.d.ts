24bb680bced2aeb6bb05de63d2cba3a07c8b76df
Same Old Nick <nick@sameoldnick.com>
2024-10-14 21:25:19 -0600

    Data can be string or object

--- resources/ts/admin/typings/notifications.d.ts
+++ resources/ts/admin/typings/notifications.d.ts
@@ -4,7 +4,7 @@ declare global {
         type: TType;
         notifiable_id: number;
         notifiable_type: string;
-        data: string;
+        data: string | object;
         created_at: string;
         updated_at: string;
         read_at: string | null;
