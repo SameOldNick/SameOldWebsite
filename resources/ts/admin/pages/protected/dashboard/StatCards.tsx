904849fd137b1bb810caab08af184f3bb8f8f9df
Same Old Nick <nick@sameoldnick.com>
2024-10-15 20:19:41 -0600

    Removed ContactMessagesCard

--- resources/ts/admin/pages/protected/dashboard/StatCards.tsx
+++ resources/ts/admin/pages/protected/dashboard/StatCards.tsx
@@ -4,13 +4,10 @@ import { Col, Row } from "reactstrap";
 import VisitorsCard from "./stat-cards/VisitorsCard";
 import BlogArticlesCard from "./stat-cards/BlogArticlesCard";
 import CommentsCard from "./stat-cards/CommentsCard";
-import ContactMessagesCard from "./stat-cards/ContactMessagesCard";
 
 import { fetchArticles } from "@admin/utils/api/endpoints/articles";
 import { loadAll } from "@admin/utils/api/endpoints/comments";
-import { all } from "@admin/utils/api/endpoints/notifications";
 import PaginateResponse from "@admin/utils/api/models/PaginateResponse";
-import Notification from "@admin/utils/api/models/notifications/Notification";
 
 import { DateTime } from "luxon";
 
@@ -45,17 +42,6 @@ const StatCards: React.FC<IStatCardsProps> = ({ visitors }) => {
         }
     }, []);
 
-    const tryFetchMessages = React.useCallback(async () => {
-        try {
-            const notifications = await all();
-
-            setCommentCount(notifications.map((record) => new Notification(record)).filter((notification) => notification.isType(Notification.NOTIFICATION_TYPE_MESSAGE)).length);
-        } catch (e) {
-            if (import.meta.env.VITE_APP_DEBUG)
-                logger.error(e);
-        }
-    }, []);
-
     React.useEffect(() => {
         if (visitors.status !== 'fulfilled')
             return;
@@ -77,7 +63,6 @@ const StatCards: React.FC<IStatCardsProps> = ({ visitors }) => {
         Promise.all([
             tryFetchArticles(),
             tryFetchComments(),
-            tryFetchMessages(),
         ]);
     }, []);
 
@@ -85,20 +70,16 @@ const StatCards: React.FC<IStatCardsProps> = ({ visitors }) => {
 
     return (
         <Row>
-            <Col md={6} xl={3} className="mb-4">
+            <Col md={12} xl={4} className="mb-4">
                 <VisitorsCard count={visitorsCount} />
             </Col>
 
-            <Col md={6} xl={3} className="mb-4">
+            <Col md={12} xl={4} className="mb-4">
                 <BlogArticlesCard articles={articleCount} />
             </Col>
 
-            <Col md={6} xl={3} className="mb-4">
+            <Col md={12} xl={4} className="mb-4">
                 <CommentsCard comments={commentCount} />
-            </Col>
-
-            <Col md={6} xl={3} className="mb-4">
-                <ContactMessagesCard messages={0} />
             </Col>
         </Row>
     );
