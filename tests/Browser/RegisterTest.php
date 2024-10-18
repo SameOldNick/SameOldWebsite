b1e234c393934f248fa8df463c7d1d891ec27ec4
Same Old Nick <nick@sameoldnick.com>
2024-10-17 23:56:10 -0600

    Added register user Dusk tests

---
+++ tests/Browser/RegisterTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Models\Article;
+use App\Models\Page;
+use Illuminate\Foundation\Testing\DatabaseTruncation;
+use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Dusk\Browser;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\Browser\Components\Alerts;
+use Tests\Browser\Components\SweetAlerts;
+use Tests\Browser\Pages\Blog\Article as BlogArticle;
+use Tests\Browser\Pages\RegisterPage;
+use Tests\DuskTestCase;
+
+class RegisterTest extends DuskTestCase
+{
+    use DatabaseTruncation;
+    use WithFaker;
+
+    /**
+     * Tests registering user
+     */
+    #[Test]
+    public function register_user(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(new RegisterPage)
+                ->assertGuest()
+                ->type('email', $this->faker->email)
+                ->type('password', $this->faker->strongPassword)
+                ->select('country', 'CAN') // Only certain countries are available in testing
+                ->check('terms_conditions')
+                ->press('Register')
+                ->waitForRoute('user.profile');
+
+            $browser
+                ->assertUrlIs(route('user.profile'))
+                ->with(new SweetAlerts, function (Browser $browser) {
+                    $browser
+                        ->assertHasSweetAlerts()
+                        ->assertSweetAlertExists(['title' => 'Registered']);
+                });
+        });
+    }
+
+    /**
+     * Tests registering user with invalid country
+     */
+    #[Test]
+    public function register_user_invalid_country(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(new RegisterPage)
+                ->assertGuest()
+                ->type('email', $this->faker->email)
+                ->type('password', $this->faker->strongPassword)
+                ->select('country', 'XYZ')
+                ->check('terms_conditions')
+                ->press('Register');
+
+            $browser
+                ->assertUrlIs(route('register'))
+                ->with(new Alerts, function (Browser $browser) {
+                    $browser
+                        ->assertHasAlerts()
+                        ->assertHasAlert('danger', __('The selected country is invalid.'));
+                });
+        });
+    }
+}
