39a705918bff989fccf7a84e8ce773569b2a332b
Same Old Nick <nick@sameoldnick.com>
2024-10-19 01:08:08 -0600

    Fixed up verify page

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -10,7 +10,7 @@
                         <h2 class="text-center">{{ __('Verify Your Email Address') }}</h2>
                     </div>
                     <div class="card-body">
-                        <div class="px-5 py-4">
+                        <div class="px-1 py-4">
                             @if (session('resent'))
                                 <x-alert type="success">
                                     {{ __('A fresh verification link has been sent to your email address.') }}
@@ -21,13 +21,13 @@
                                 {{ __('Before proceeding, please check your email for a verification link.') }}
                             </p>
 
-                            <p class="text-center">
+                            <div class="text-center">
                                 {{ __('If you did not receive the email') }},
                                 <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                                     @csrf
-                                    <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('Click here to request another') }}</button>.
+                                    <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
                                 </form>
-                            </p>
+                            </div>
                         </div>
                     </div>
                 </div>
