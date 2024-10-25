a33488cb448bf6d742b26e5cc2958d669b8985b6
SameOldNick <SameOldNick@users.noreply.github.com>
2024-10-25 06:29:01 +0000

    Applied Laravel Pint changes

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -6,7 +6,6 @@ use App\Models\Collections\RoleCollection;
 use App\Traits\Models\HidesPrimaryKey;
 use Illuminate\Database\Eloquent\Attributes\CollectedBy;
 use Illuminate\Database\Eloquent\Casts\Attribute;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
