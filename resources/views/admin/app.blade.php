38906cb923b97d3eed34a1346bba90f4cca3952f
Same Old Nick <nick@sameoldnick.com>
2024-10-31 01:11:58 -0600

    Removed duplicate meta tags

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -4,7 +4,6 @@
 <x-code-credit />
 
         <meta charset="utf-8">
-        <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
