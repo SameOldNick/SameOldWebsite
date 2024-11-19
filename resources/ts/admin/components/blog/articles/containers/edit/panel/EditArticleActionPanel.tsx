e538584786ba41618c80242874dcc8bc4ceebcd6
Nick Hamnett <nick@sameoldnick.com>
2024-11-19 17:20:52 +0200

    Added onLoading and onError helpers

--- resources/ts/admin/components/blog/articles/containers/edit/panel/EditArticleActionPanel.tsx
+++ resources/ts/admin/components/blog/articles/containers/edit/panel/EditArticleActionPanel.tsx
@@ -5,6 +5,8 @@ import withReactContent from 'sweetalert2-react-content';
 import { FormikProps } from 'formik';
 import Swal, { SweetAlertOptions, SweetAlertResult } from 'sweetalert2';
 
+import Loader from '@admin/components/Loader';
+
 import EditArticleActionPanelContext, { EditArticleActionsContextValues } from '@admin/components/blog/articles/containers/edit/panel/EditArticleActionPanelContext';
 import useArticleEditActions from '@admin/components/blog/articles/containers/edit/hooks/useArticleEditActions';
 import { ArticleFormValues } from '@admin/components/blog/articles/containers/formik/ArticleFormikProvider';
@@ -18,7 +20,9 @@ import Article from '@admin/utils/api/models/Article';
 import Revision from '@admin/utils/api/models/Revision';
 
 type ArticleEditActionsHandlerHelpers = {
+    onLoading: (loading: boolean) => void;
     onSuccess: (message: string, extra?: SweetAlertOptions) => Promise<SweetAlertResult>;
+    onError: (message: string, extra?: SweetAlertOptions) => Promise<SweetAlertResult>;
     onNavigate: (url: string) => void;
     onReload: () => void;
     onUpdate: () => void;
@@ -58,18 +62,27 @@ interface EditArticleActionPanelProps {
 const EditArticleActionPanel: React.FC<EditArticleActionPanelProps> = ({ article, revision, formik }) => {
     const navigate = useNavigate();
 
+    const [loading, setLoading] = React.useState(false);
+
     const helpers = React.useMemo<ArticleEditActionsHandlerHelpers>(() => ({
+        onLoading: (loading: boolean) => {
+            setLoading(loading);
+        },
         onUpdate: () => {
             formik.resetForm();
         },
-        onSuccess: (message: string, extra: SweetAlertOptions = {}) => {
-            return withReactContent(Swal).fire({
-                title: 'Success',
-                text: message,
-                icon: 'success',
-                ...extra
-            });
-        },
+        onSuccess: (message: string, extra: SweetAlertOptions = {}) => withReactContent(Swal).fire({
+            title: 'Success',
+            text: message,
+            icon: 'success',
+            ...extra
+        }),
+        onError: (message: string, extra: SweetAlertOptions = {}) => withReactContent(Swal).fire({
+            title: 'Oops...',
+            text: message,
+            icon: 'error',
+            ...extra
+        }),
         onNavigate: (url: string) => {
             navigate(url);
         },
@@ -115,6 +128,8 @@ const EditArticleActionPanel: React.FC<EditArticleActionPanelProps> = ({ article
     return (
         <>
             <EditArticleActionPanelContext.Provider value={contextValue}>
+                {loading && <Loader display={{ type: 'page', show: true }} />}
+
                 <ArticleInformationAction />
 
                 <RestoreRevisionAction />
