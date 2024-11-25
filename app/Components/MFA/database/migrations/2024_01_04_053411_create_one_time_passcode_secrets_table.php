be63490f58aca9950b8535fe65131c7197652e39
Nick Hamnett <nick@sameoldnick.com>
2024-11-25 23:17:16 +0000

    Fixed table name

--- app/Components/MFA/database/migrations/2024_01_04_053411_create_one_time_passcode_secrets_table.php
+++ app/Components/MFA/database/migrations/2024_01_04_053411_create_one_time_passcode_secrets_table.php
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('totp_secrets');
+        Schema::dropIfExists('one_time_passcode_secrets');
     }
 };
