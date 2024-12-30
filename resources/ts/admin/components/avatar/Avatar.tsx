5e2c3a4b663c17c17a20b20a15600c4d52f2c5ee
Same Old Nick <nick@sameoldnick.com>
2024-12-30 01:05:49 -0700

    Moved wrapper components to wrappers folder

--- resources/ts/admin/components/avatar/Avatar.tsx
+++ resources/ts/admin/components/avatar/Avatar.tsx
@@ -3,7 +3,7 @@ import { ClipLoader } from 'react-spinners';
 
 import classNames from 'classnames';
 
-import LazyLoadImage from '@admin/components/hoc/LazyLoadImage';
+import LazyLoadImage from '@admin/components/wrappers/LazyLoadImage';
 
 interface IAvatarProps extends Omit<React.HTMLProps<HTMLImageElement>, 'ref' | 'placeholder' | 'onError'> {
     src: string;
