947dca2c8da09c67477eaf544aba08f252ff9290
Same Old Nick <nick@sameoldnick.com>
2025-01-06 17:10:20 -0700

    Uses arif-rh/profanity-filter package

--- app/Components/Moderator/Moderators/Comments/ProfanityModerator.php
+++ app/Components/Moderator/Moderators/Comments/ProfanityModerator.php
@@ -6,10 +6,7 @@ use App\Components\Moderator\Concerns\CompilesList;
 use App\Components\Moderator\Contracts\Moderator;
 use App\Components\Moderator\Exceptions\FlagCommentException;
 use App\Models\Comment;
-use Illuminate\Support\Str;
-
-use function Safe\preg_match;
-use function Safe\preg_replace;
+use Arifrh\ProfanityFilter\Check;
 
 /**
  * @implements Moderator<Comment>
@@ -35,46 +32,13 @@ class ProfanityModerator implements Moderator
      */
     public function moderate($comment): void
     {
-        $profanity = $this->findProfanity($comment->comment);
+        $check = new Check($this->getWords());
 
-        if (empty($profanity)) {
-            return;
+        $cleaned = $check->cleanWords($comment->comment);
+
+        if ($cleaned !== $comment->comment) {
+            throw new FlagCommentException('Comments cannot contain profanity.', $cleaned);
         }
-
-        $patterns = array_map(fn ($word) => sprintf('/%s/i', preg_quote($word)), $profanity);
-
-        $updated = preg_replace($patterns, '[redacted]', $comment->comment);
-
-        throw new FlagCommentException('Comments cannot contain profanity.', $updated);
-    }
-
-    /**
-     * Change characters to UTF8 version
-     */
-    protected function normalizeText(string $text): string
-    {
-        return (string) Str::of($text)->ascii();
-    }
-
-    /**
-     * Searches for matching profanity
-     *
-     * @return array Found words
-     */
-    protected function findProfanity(string $comment): array
-    {
-        $comment = strtolower($comment);
-        $found = [];
-
-        foreach ($this->getWords() as $word) {
-            $pattern = sprintf('/\s%s\s/i', preg_quote($word));
-
-            if (preg_match($pattern, $word)) {
-                array_push($found, $word);
-            }
-        }
-
-        return $found;
     }
 
     /**
