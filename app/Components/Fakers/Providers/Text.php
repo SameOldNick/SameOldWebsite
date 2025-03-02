644e77fefc54986f767254f2c77f13d253a28d44
Same Old Nick <nick@sameoldnick.com>
2025-03-01 23:17:34 -0700

    Loads profanity using require

--- app/Components/Fakers/Providers/Text.php
+++ app/Components/Fakers/Providers/Text.php
@@ -5,9 +5,11 @@ namespace App\Components\Fakers\Providers;
 use App\Components\Passwords\Generator\Options;
 use App\Components\Passwords\Password;
 use Faker\Provider\Base;
+use Illuminate\Support\Arr;
 
 class Text extends Base
 {
+
     /**
      * Generates a strong password.
      *
@@ -26,7 +28,8 @@ class Text extends Base
      */
     public function profanity(int $count = 1, $lang = 'en')
     {
-        $words = config("profanity.{$lang}", []);
+        $lists = require app_path('Components/Moderator/data/profanity.php');
+        $words = Arr::get($lists, $lang, []);
 
         return $count === 1 ? $this->randomElement($words) : $this->randomElements($words, $count);
     }
