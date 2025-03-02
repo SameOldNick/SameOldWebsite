43d33db37eff8d0405f4133d131443d7eea1c931
Same Old Nick <nick@sameoldnick.com>
2025-03-01 23:20:42 -0700

    Fixes errors not being displayed

--- app/Http/Controllers/Main/BlogArticleController.php
+++ app/Http/Controllers/Main/BlogArticleController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\Main;
 
 use App\Http\Controllers\Controller;
+use App\Http\Middleware\ErrorsToSweetAlert;
 use App\Models\Article;
 use App\Models\Revision;
 use Illuminate\Http\Request;
 
 class BlogArticleController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware([ErrorsToSweetAlert::class]);
+    }
+
     /**
      * Display the specified article.
      *
