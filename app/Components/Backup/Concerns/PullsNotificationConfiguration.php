db2653032cb840dc8c76929ff1e8d4fbcdb3f9b1
Same Old Nick <nick@sameoldnick.com>
2025-12-14 19:41:55 -0700

    Add docblocks to notification configuration methods
    Added PHPDoc comments to methods in the PullsNotificationConfiguration trait to clarify their purpose and improve code readability.

--- app/Components/Backup/Concerns/PullsNotificationConfiguration.php
+++ app/Components/Backup/Concerns/PullsNotificationConfiguration.php
@@ -11,26 +11,41 @@ trait PullsNotificationConfiguration
 {
     use ApplyNotificationConfiguration;
 
+    /**
+     * Get the notification channels.
+     */
     public function via(): array
     {
         return $this->getConfigurationProvider()->channels(static::class);
     }
 
+    /**
+     * Apply configuration to each mail notification channel message.
+     */
     public function toMail(): MailMessage
     {
         return $this->applyConfigurationToMailMessage(parent::toMail());
     }
 
+    /**
+     * Apply configuration to each Slack notification channel message.
+     */
     public function toSlack(): SlackMessage
     {
         return $this->applyConfigurationToSlackMessage(parent::toSlack());
     }
 
+    /**
+     * Apply configuration to each Discord notification channel message.
+     */
     public function toDiscord(): DiscordMessage
     {
         return $this->applyConfigurationToDiscordMessage(parent::toDiscord());
     }
 
+    /**
+     * Apply configuration to each Ntfy notification channel message.
+     */
     public function toNtfy($notifiable): NtfyMessage
     {
         return $this->applyConfigurationToNtfyMessage(parent::toNtfy($notifiable));
